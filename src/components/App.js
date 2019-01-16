4e6c57bf0d581475266a009eb850d1760482f00f
buffalorob <peckham.rob@gmail.com>
2019-01-16 15:07:38 -0500

    App: + Route for ItemCreate

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { Router, Route } from 'react-router-dom';
 import ItemList from './items/ItemList';
 import Header from './Header';
 import history from '../history';
+import ItemCreate from './items/ItemCreate';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <div>
         <Header />
         <Route path='/' exact component={ItemList} />
+        <Route path="/items/new" exact component={ItemCreate} />
       </div>
     </Router>
   )
