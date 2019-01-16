163450bf8aa7f55ddc1163e5985704d800e6a0fa
buffalorob <peckham.rob@gmail.com>
2019-01-16 15:08:44 -0500

    ItemForm: Basic form + validation

--- src/components/items/ItemForm.js
+++ src/components/items/ItemForm.js
@@ -1,9 +1,38 @@
 import React from "react";
+import { Field, reduxForm } from 'redux-form'
 
 class ItemForm extends React.Component {
+
+  renderInput = ({ input, label }) => {
+    return (
+      <div>
+        <label>{label}</label>
+        <input {...input} autoComplete='off' />
+      </div>
+    )
+  }
+
   render() {
-    return <div>ItemList</div>;
+    return (
+      <form>
+        <Field name='itemName' component={this.renderInput} label='Enter a New Item Name' />
+        <button>Submit</button>
+      </form>
+    );
   }
 }
 
-export default ItemForm;
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.itemName) {
+    errors.itemName = "You Must Enter an Item Name to Make a New Item"
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  form: 'itemForm',
+  validate: validate
+})(ItemForm);
