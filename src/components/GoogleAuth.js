9be77a9f15b4d4efd5abe6f135e327e4950c7bc6
buffalorob <peckham.rob@gmail.com>
2019-01-18 13:09:46 -0500

    GoogleAuth: + logic to determine if signed in

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,21 +2,38 @@ import React from 'react';
 
 //TODO: Check to make sure the client id used on line 8 doesn't need to be hidden in some way
 
+
+// Go to lecture 211
+
 class GoogleAuth extends React.Component {
+
+  state = { isSignedIn: null };
 
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: '915420385972-j4d1d449764jfasb8jagcufj4jd71h7h.apps.googleusercontent.com',
         scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
       });
     });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
   }
 
   render() {
     return (
       <div>
-        GoogleAuth
+        {this.renderAuthButton()}
       </div>
     );
   };
