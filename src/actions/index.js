ee659cba6d0afc3e73cebb9bf20e920dc29d78b9
BuffaloRob <peckham.rob@gmail.com>
2019-01-19 11:49:14 -0500

    actions: + basic actions for signing in and out

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 import history from '../history';
+
+export const signIn = () => {
+  return {
+    type: 'SIGN_IN'
+  };
+};
+
+export const signOut = () => {
+  return {
+    type: 'SIGN_OUT'
+  };
+};
