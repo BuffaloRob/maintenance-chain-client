eda51b2cb94f6843109fe3114cc37f6bb462b12d
BuffaloRob <peckham.rob@gmail.com>
2019-01-19 12:27:43 -0500

    GoogleAuth: +mapStateToProps to used state of isSignedIn

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -25,7 +25,7 @@ class GoogleAuth extends React.Component {
 
   onAuthChange = isSignedIn => {
     if (isSignedIn) {
-      this.props.signIn();
+      this.props.signIn(this.auth.currentUser.get().getId());
     } else {
       this.props.signOut();
     }
@@ -64,4 +64,9 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default connect(null, { signIn, signOut })(GoogleAuth);
+const mapStateToProps = (state) => {
+  // (state)= current state (.)(auth)= key used in combineReducers (.)(isSignedIn) = key used in authReducer
+  return { isSignedIn: state.auth.isSignedIn };
+}
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
