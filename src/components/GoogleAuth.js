76969acf770130cbe1bbf36750c3e3651c34920c
buffalorob <peckham.rob@gmail.com>
2019-01-22 13:39:33 -0500

    GoogleA: changed setState to onAuthChange. replaced all .state w/ .props

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -17,7 +17,7 @@ class GoogleAuth extends React.Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.onAuthChange(this.auth.isSignedIn.get());
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
@@ -40,9 +40,9 @@ class GoogleAuth extends React.Component {
   };
 
   renderAuthButton() {
-    if (this.state.isSignedIn === null) {
+    if (this.props.isSignedIn === null) {
       return null;
-    } else if (this.state.isSignedIn) {
+    } else if (this.props.isSignedIn) {
       return (
         <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon" />
