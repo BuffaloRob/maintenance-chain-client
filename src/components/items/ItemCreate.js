4aec77e5304fb5178cb1f62b9d9ae2182c0c1e20
buffalorob <peckham.rob@gmail.com>
2019-01-23 14:10:18 -0500

    ItemCreate: imported connect & createItem

--- src/components/items/ItemCreate.js
+++ src/components/items/ItemCreate.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { connect } from 'react-redux';
 
 import ItemForm from './ItemForm';
+import { createItem } from '../../actions';
 //need to import Connect() and wire up to action creator
 
 class ItemCreate extends React.Component {
