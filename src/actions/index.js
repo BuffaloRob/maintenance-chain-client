cf4f2c6b62cfc9bdc3f74d4e305e1d2f85567eb7
buffalorob <peckham.rob@gmail.com>
2019-01-24 08:48:07 -0500

    actions: + fetchItems, fetchItem, editItem, deleteItem

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,7 @@ import {
   FETCH_ITEM,
   FETCH_ITEMS,
   DELETE_ITEM,
-  EDIT_ITEM ,
+  EDIT_ITEM,
 } from './types';
 
 export const signIn = userId => {
@@ -25,8 +25,26 @@ export const signOut = () => {
 
 export const createItem = formValues => async dispatch => {
   const response = await maintenance.post('/maintenance', formValues);
-  dispatch({ type: CREATE_ITEM, payload: response.data});
+  dispatch({ type: CREATE_ITEM, payload: response.data });
 
 };
 
-//lecture 240 at 5:50
+export const fetchItems = () => async dispatch => {
+  const response = await maintenance.get('/maintenance');
+  dispatch({ type: FETCH_ITEMS, payload: response.data });
+}
+
+export const fetchItem = id => async dispatch => {
+  const response = await maintenance.get(`/maintenance/${id}`);
+  dispatch({ type: FETCH_ITEM, payload: response.data });
+}
+
+export const editItem = (id, formValues) => async dispatch => {
+  const response = await maintenance.put(`/maintenance/${id}`, formValues);
+  dispatch({ type: EDIT_ITEM, payload: response.data });
+}
+
+export const deleteItem = id => async dispatch => {
+  await maintenance.delete(`/streams/${id}`);
+  dispatch({ type: DELETE_ITEM, payload: id });
+}
