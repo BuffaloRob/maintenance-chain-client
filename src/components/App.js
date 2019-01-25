806651f8cd769aa93ad60bab5584d2e628342741
buffalorob <peckham.rob@gmail.com>
2019-01-25 10:22:19 -0500

    App: + Route's with /:id

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,9 @@ import ItemList from './items/ItemList';
 import Header from './Header';
 import history from '../history';
 import ItemCreate from './items/ItemCreate';
+import ItemDelete from './items/ItemDelete';
+import ItemEdit from './items/ItemEdit';
+import ItemShow from './items/ItemShow';
 
 const App = () => {
   return <div className="ui container">
@@ -13,6 +16,9 @@ const App = () => {
           <Header />
           <Route path="/" exact component={ItemList} />
           <Route path="/items/new" exact component={ItemCreate} />
+          <Route path="/items/:id" exact component={ItemShow} />
+          <Route path="/items/delete/:id" exact component={ItemDelete} />
+          <Route path="/items/edit/:id" exact component={ItemEdit} />
         </div>
       </Router>
     </div>;
