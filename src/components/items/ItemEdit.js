1d0c50117cf04237873cfab942653f388c95ed11
buffalorob <peckham.rob@gmail.com>
2019-01-25 11:20:08 -0500

    ItemEdit: + mapStateToProps & wired up connect()

--- src/components/items/ItemEdit.js
+++ src/components/items/ItemEdit.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from 'react-redux';
 
 class ItemEdit extends React.Component {
   render() {
@@ -6,4 +7,8 @@ class ItemEdit extends React.Component {
   }
 }
 
-export default ItemEdit;
+const mapStateToProps = (state, ownProps) => {
+  return { item: state.items[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps)(ItemEdit);
