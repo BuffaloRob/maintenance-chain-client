be9b3d711578d7c0427a4b54d4bdff656a8ba732
buffalorob <peckham.rob@gmail.com>
2019-01-25 19:08:22 -0500

    ItemEdit: +compDidMount to auto load data, now displays itemName

--- src/components/items/ItemEdit.js
+++ src/components/items/ItemEdit.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { connect } from 'react-redux';
+import { fetchItem } from '../../actions';
 
 class ItemEdit extends React.Component {
-  render() {
-    return <div>ItemEdit</div>;
+
+  componentDidMount() {
+    this.props.fetchItem(this.props.match.params.id);
   }
+
+  render() {
+  if (!this.props.item.itemName) {
+    return <div>Loading...</div>
+  }
+
+  return <div>{this.props.item.itemName}</div>
+  }
+   
 }
+
 
 const mapStateToProps = (state, ownProps) => {
   return { item: state.items[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps)(ItemEdit);
+export default connect(mapStateToProps, { fetchItem })(ItemEdit);
