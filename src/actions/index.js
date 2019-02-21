1e9aebbb04d19327270139b871ade30ed785e627
BuffaloRob <peckham.rob@gmail.com>
2019-02-21 13:23:12 -0500

    + signup & authenticate actions using axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,43 +2,62 @@ import history from '../history';
 import apiURL from '../apis/maintenance';
 import * as types from './types';
 
-export const signIn = userId => {
-  return {
-    type: types.SIGN_IN,
-    payload: userId
-  };
-};
 
-export const signOut = () => {
-  return {
-    type: types.SIGN_OUT
-  };
-};
+export const signup = user => async dispatch => {
+  const newUser = user
+  const axiosConfig = {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({user: user})
+  }
+  const response = await apiURL.post('/users', axiosConfig);
+  dispatch(authenticate({
+    name: newUser.name,
+    email: newUser.email,
+    password: newUser.password,
+    payload: response.data})
+  );
+}
+
+export const authenticate = credentials => async dispatch => {
+  const axiosConfig = {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ auth: credentials })
+  }
+  dispatch(authRequest());
+  const response = await apiURL.post('/user_token', axiosConfig);
+  // to be continued ...
+
+}
 
 export const createItem = formValues => async (dispatch, getState) => {
-  const { userId } = getState().auth;
-  const response = await apiURL.post('/API_URL', { ...formValues, userId });
+  const { userId } = getState().items;
+  const response = await apiURL.post('/items', { ...formValues, userId });
   dispatch({ type: types.CREATE_ITEM, payload: response.data });
   history.push('/');
 };
 
 export const fetchItems = () => async dispatch => {
-  const response = await apiURL.get('/apiURL');
+  const response = await apiURL.get('/items');
   dispatch({ type: types.FETCH_ITEMS, payload: response.data });
 }
 
 export const fetchItem = id => async dispatch => {
-  const response = await apiURL.get(`/apiURL/${id}`);
+  const response = await apiURL.get(`/items/${id}`);
   dispatch({ type: types.FETCH_ITEM, payload: response.data });
 }
 
 export const editItem = (id, formValues) => async dispatch => {
-  const response = await apiURL.put(`/apiURL/${id}`, formValues);
+  const response = await apiURL.put(`/items/${id}`, formValues);
   dispatch({ type: types.EDIT_ITEM, payload: response.data });
 }
 
 export const deleteItem = id => async dispatch => {
-  await apiURL.delete(`/apiURL/${id}`);
+  await apiURL.delete(`/items/${id}`);
   dispatch({ type: types.DELETE_ITEM, payload: id });
 }
 
@@ -62,3 +81,16 @@ export const authFailure = errors => {
     errors: errors
   }
 }
+
+export const signIn = userId => {
+  return {
+    type: types.SIGN_IN,
+    payload: userId
+  };
+};
+
+export const signOut = () => {
+  return {
+    type: types.SIGN_OUT
+  };
+};
