5dcee4603ff9e032b8c103027f265410ed1b9819
BuffaloRob <peckham.rob@gmail.com>
2019-02-21 10:09:57 -0500

    + api to baseURL for api call

--- src/apis/maintenance.js
+++ src/apis/maintenance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Set-up for the json-api server
 export default axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'http://localhost:3001/api'
 })
 
 // TODO: set up connection to rails api
