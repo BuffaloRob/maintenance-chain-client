6d3485ec5b20d074e3c2250727a9f013ad01b23e
BuffaloRob <peckham.rob@gmail.com>
2019-02-22 10:26:02 -0500

    App: + route for SignUp

--- src/components/App.js
+++ src/components/App.js
@@ -9,16 +9,19 @@ import ItemDelete from './items/ItemDelete';
 import ItemEdit from './items/ItemEdit';
 import ItemShow from './items/ItemShow';
 
+import SignUp from './SignUp';
+
 const App = () => {
   return <div className="ui container">
       <Router history={history}>
         <div>
           <Header />
-          <Route path="/" exact component={ItemList} />
+          <Route exact path="/" component={ItemList} />
           <Route path="/items/new" exact component={ItemCreate} />
           <Route path="/items/:id" exact component={ItemShow} />
           <Route path="/items/delete/:id" exact component={ItemDelete} />
           <Route path="/items/edit/:id" exact component={ItemEdit} />
+          <Route exact path="/signup" component={SignUp} />
         </div>
       </Router>
     </div>;
