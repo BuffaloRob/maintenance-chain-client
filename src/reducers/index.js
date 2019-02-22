08cd600158181a2e98aea6b85822a2b637444cb1
BuffaloRob <peckham.rob@gmail.com>
2019-02-22 11:09:44 -0500

    combineReducers: imported googleReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import { reducer as formReducer } from 'redux-form';
 
 import authReducer from './authReducer';
 import itemReducer from './itemReducer';
+import googleReducer from './googleReducer';
 
 export default combineReducers({
   auth: authReducer,
