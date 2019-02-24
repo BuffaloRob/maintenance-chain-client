d58c724bbe001b030e9ce7d234db196d7936dc59
BuffaloRob <peckham.rob@gmail.com>
2019-02-23 20:28:00 -0500

    actions: + authenticate, signup, getUser

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,36 +2,78 @@ import history from '../history';
 import apiURL from '../apis/maintenance';
 import * as types from './types';
 
+const API_URL = "http://localhost:3001/api"
 
-export const signup = user => async dispatch => {
+// Using fetch to satisfy project requirements
+export const signup = (user) => {
   const newUser = user
-  const axiosConfig = {
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({user: user})
-  }
-  const response = await apiURL.post('/users', axiosConfig);
-  dispatch(authenticate({
-    name: newUser.name,
-    email: newUser.email,
-    password: newUser.password,
-    payload: response.data})
-  );
+  return dispatch => {
+    return fetch(`${API_URL}/users`, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ user: user })
+    })
+      .then(response => response.json())
+      .then(jresp => {
+        dispatch(authenticate({
+          name: newUser.name,
+          email: newUser.email,
+          password: newUser.password
+        })
+        );
+      })
+      .catch((errors) => {
+        dispatch(authFailure(errors))
+      })
+  };
 }
 
-export const authenticate = credentials => async dispatch => {
-  const axiosConfig = {
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ auth: credentials })
+// Using fetch to satisfy project requirements
+export const authenticate = (credentials) => {
+  return dispatch => {
+    dispatch(authRequest())
+    return fetch(`${API_URL}/user_token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ auth: credentials })
+    })
+      .then(res => res.json())
+      .then((response) => {
+        const token = response.jwt;
+        localStorage.setItem('token', token);
+        return getUser(credentials)
+      })
+      .then((user) => {
+        console.log(user)
+        dispatch(authSuccess(user, localStorage.token))
+      })
+      .catch((errors) => {
+        dispatch(authFailure(errors))
+        localStorage.clear()
+      })
   }
-  dispatch(authRequest());
-  const response = await apiURL.post('/user_token', axiosConfig);
-  // to be continued
+}
 
+export const getUser = (credentials) => {
+  const request = new Request(`${API_URL}/find_user`, {
+    method: "POST",
+    headers: new Headers({
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${localStorage.token}`,
+    }),
+    body: JSON.stringify({ user: credentials })
+  })
+  return fetch(request)
+    .then(response => response.json())
+    .then(userJson => { return userJson })
+    .catch(error => {
+      return error;
+    });
 }
 
 export const createItem = formValues => async (dispatch, getState) => {
@@ -94,3 +136,35 @@ export const signOut = () => {
     type: types.SIGN_OUT
   };
 };
+
+
+// export const signup = user => async dispatch => {
+//   const newUser = user
+//   const axiosConfig = {
+//     headers: {
+//       "Accept": "application/json",
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({user: user})
+//   }
+//   const response = await apiURL.post('/users', axiosConfig);
+//   dispatch(authenticate({
+//     name: newUser.name,
+//     email: newUser.email,
+//     password: newUser.password,
+//     payload: response.data})
+//   );
+// }
+
+// export const authenticate = credentials => async dispatch => {
+//   const axiosConfig = {
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({ auth: credentials })
+//   }
+//   dispatch(authRequest());
+//   const response = await apiURL.post('/user_token', axiosConfig);
+//   // to be continued
+
+// }
