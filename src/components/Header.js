199eece4d025066e74c3344efb70ef4926887af7
BuffaloRob <peckham.rob@gmail.com>
2019-02-27 13:19:09 -0500

    Header: turned into react component, + logout ability

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Welcome from './Welcome';
+import { logout } from '../actions/authActions';
 
-const Header = () => {
-  return (
-    <div className='ui secondary pointing menu'>
-      <Link to='/' className='item'>
-        M/C
-      </Link>
-      <div className='right menu'>
+class Header extends React.Component {
+
+  handleLogout = e => {
+    e.preventDefault();
+    this.props.logout();
+  }
+
+  render() {
+    return (
+      <div className='ui secondary pointing menu'>
         <Link to='/' className='item'>
-          All Items
+          M/C
         </Link>
-        <Link to='/signup' className='item'>
-          Sign Up
-        </Link>
-        <Link to='/login' className='item'>
-          Log In
-        </Link>
-        <Welcome />
+        <div className='right menu'>
+          <Link to='/' className='item'>
+            All Items
+          </Link>
+          <Link to='/signup' className='item'>
+            Sign Up
+          </Link>
+          <Link to='/login' className='item'>
+            Log In
+          </Link>
+          <li onClick={(e) => this.handleLogout(e)}className='item pointing'>Log out</li>
+          <Welcome />
+        </div>
+
       </div>
-     
-    </div>
-  )
+    )
+  }
+  
 }
 
-export default Header;
+export default connect(null, { logout })(Header);
