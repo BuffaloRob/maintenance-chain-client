751f2d39852f02e770db205154543ed3819946b0
BuffaloRob <peckham.rob@gmail.com>
2019-03-03 09:48:02 -0500

    ItemDelete: + basic item delete that uses Modal

--- src/components/items/ItemDelete.js
+++ src/components/items/ItemDelete.js
@@ -1,9 +1,55 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { fetchItem, deleteItem } from '../../actions/itemActions';
+import Modal from '../Modal';
+import history from '../../history';
+
 
 class ItemDelete extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchItem(this.props.match.params.id);
+  }
+
+  renderActions() {
+    const { id } = this.props.match.params;
+    return(
+      <>
+        <button
+          onClick={() => this.props.deleteItem(id)}
+          className='ui button negative'
+        >
+          Delete
+        </button>
+        <Link to='/' className='ui button'>Cancel</Link>
+      </>
+    );
+  }
+
+  renderContent() {
+    if(!this.props.item) {
+      return 'Are you sure you want to delete this Item?'
+    }
+
+    return `Are you sure you want to delete the Item: ${this.props.item.name}?`
+  }
+
   render() {
-    return <div>ItemDelete</div>;
+    return (
+      <Modal 
+        title='Delete Item'
+        content={this.renderContent()}
+        actions={this.renderActions()}
+        onDismiss={() => history.push('/')}
+      />
+    );
   }
 }
 
-export default ItemDelete;
+const mapStateToProps = (state, ownProps) => {
+  return { item: state.items[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchItem, deleteItem })(ItemDelete);
