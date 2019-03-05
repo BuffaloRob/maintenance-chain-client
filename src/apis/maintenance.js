9361f93b299d339ca532fa48bc467046437e5838
BuffaloRob <peckham.rob@gmail.com>
2019-03-05 10:09:27 -0500

    api: + headers to axios.create()

--- src/apis/maintenance.js
+++ src/apis/maintenance.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // Set-up for the json-api server
 export default axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: 'http://localhost:3000/api',
+  headers: { Authorization: `Bearer ${localStorage.token}` }
 })
 
 // TODO: set up connection to rails api
