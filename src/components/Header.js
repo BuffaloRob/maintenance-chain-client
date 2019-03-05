e21c398ca284c7837e2b4f26d7c0ef628dd2da65
BuffaloRob <peckham.rob@gmail.com>
2019-03-05 13:36:12 -0500

    Header: now renders different routes if user authenticated

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,32 @@ class Header extends React.Component {
     this.props.logout();
   }
 
+  renderAuthenticated() {
+    if (this.props.isAuthenticated) {
+      return (
+        <>
+          <Link to='/items/new' className='item'>
+            New Item
+          </Link>
+          <li onClick={(e) => this.handleLogout(e)} className='item pointing'>Log out</li>
+          <Welcome />
+        </>
+      ) 
+    } else {
+      return (
+          <>
+            <Link to='/signup' className='item'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='item'>
+              Log In
+            </Link>
+            <Welcome />
+          </>
+      )
+    }
+  }
+
   render() {
     return (
       <div className='ui secondary pointing menu'>
@@ -19,23 +45,16 @@ class Header extends React.Component {
           M/C
         </Link>
         <div className='right menu'>
-          <Link to='/items/new' className='item'>
-            New Item
-          </Link>
-          <Link to='/signup' className='item'>
-            Sign Up
-          </Link>
-          <Link to='/login' className='item'>
-            Log In
-          </Link>
-          <li onClick={(e) => this.handleLogout(e)}className='item pointing'>Log out</li>
-          <Welcome />
+          {this.renderAuthenticated()}
         </div>
-
       </div>
     )
   }
   
 }
 
-export default connect(null, { logout })(Header);
+const mapStateToProps = state => {
+  return { isAuthenticated: state.auth.isAuthenticated }
+}
+
+export default connect(mapStateToProps, { logout })(Header);
