4cf88c35cfcee1a3868aa977def225b0c999a69a
BuffaloRob <peckham.rob@gmail.com>
2019-03-14 11:08:18 -0400

    maintenance: changed local to session storage

--- src/apis/maintenance.js
+++ src/apis/maintenance.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 // Set-up for the json-api server
 export default axios.create({
   baseURL: 'http://localhost:3000/api/v1',
-  headers: { Authorization: `Bearer ${localStorage.token}` }
+  headers: { Authorization: `Bearer ${sessionStorage.jwt}` }
 })
