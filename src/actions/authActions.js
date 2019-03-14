d576d04fe1d9152a23ae2fadaa1858e37226d25c
BuffaloRob <peckham.rob@gmail.com>
2019-03-14 11:05:21 -0400

    authActions: changed to match new login method of simple JWT

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,103 +1,109 @@
 import history from '../history';
-import * as types from './types';
+import { SET_USER, LOGOUT } from './types';
 
 const API_URL = "http://localhost:3000/api/v1"
 
-// Using fetch to satisfy project requirements
-export const signup = (user) => {
-  const newUser = user
-  return dispatch => {
-    return fetch(`${API_URL}/users`, {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ user: user })
-    })
-      .then(resp => resp.json())
-      .then(jresp => {
-        dispatch(authenticate({
-          email: newUser.email,
-          password: newUser.password
-        })
-        );
-      })
-      .catch((errors) => {
-        dispatch(authFailure(errors))
-      })
-  };
-}
+export const signup = (user, callback) => {
+  let data = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ user })
+  }
 
-// Using fetch to satisfy project requirements
-export const authenticate = (credentials) => {
   return dispatch => {
-    dispatch(authRequest())
-    return fetch(`${API_URL}/user_token`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ auth: credentials })
-    })
+    fetch(`${API_URL}/signup`, data)
       .then(resp => resp.json())
-      .then(jresp => {
-        const token = jresp.jwt;
-        localStorage.setItem('token', token);
-        return getUser(credentials)
-      })
-      .then((user) => {
-        dispatch(authSuccess(user, localStorage.token));
+      .then(user => {
+        sessionStorage.setItem('jwt', user.jwt)
+
+        dispatch({
+          type: SET_USER,
+          payload: user.currentUser
+        })
+        
         history.push('/');
+        callback();
       })
-      .catch((errors) => {
-        dispatch(authFailure(errors))
-        localStorage.clear()
-      })
+      .catch(err => err)
   }
 }
 
-export const getUser = (credentials) => {
-  const request = new Request(`${API_URL}/find_user`, {
-    method: "POST",
-    headers: new Headers({
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${localStorage.token}`,
-    }),
-    body: JSON.stringify({ user: credentials })
-  })
-  return fetch(request)
-    .then(resp => resp.json())
-    .then(jresp => { return jresp })
-    .catch(error => {
-      return error;
-    });
+export const login = (user, callback) => {
+  let data = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ user })
+  }
+
+  return dispatch => {
+    fetch(`${API_URL}/login`, data)
+      .then(resp => resp.json())
+      .then(user => {
+        sessionStorage.setItem('jwt', user.jwt)
+
+        dispatch({
+          type: SET_USER,
+          payload: user.currentUser
+        })
+
+        history.push("/");
+        callback()
+      })
+      .catch(err => err)
+  }
+}
+
+// export const getUser = (credentials) => {
+//   const request = new Request(`${API_URL}/find_user`, {
+//     method: "POST",
+//     headers: new Headers({
+//       "Content-Type": "application/json",
+//       "Authorization": `Bearer ${localStorage.token}`,
+//     }),
+//     body: JSON.stringify({ user: credentials })
+//   })
+//   return fetch(request)
+//     .then(resp => resp.json())
+//     .then(jresp => { return jresp })
+//     .catch(error => {
+//       return error;
+//     });
+// }
+
+export const fetchUser = () => {
+  let data = {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': sessionStorage.jwt
+    }
+  }
+
+  return dispatch => {
+    fetch(`${API_URL}/user`, data)
+      .then(resp => resp.json())
+      .then(user => {
+        dispatch({
+          type: SET_USER,
+          payload: user
+        })
+      })
+      .catch(err => err)
+  }
 }
 
 export const logout = () => {
   return dispatch => {
-    localStorage.clear();
+    sessionStorage.clear();
     history.push('/');
-    return dispatch({ type: types.LOGOUT });
+    return dispatch({ type: LOGOUT });
     
-  }
-}
-
-export const authRequest = () => {
-  return { type: types.AUTHENTICATION_REQUEST }
-}
-
-export const authSuccess = (user, token) => {
-  return {
-    type: types.AUTHENTICATION_SUCCESS,
-    user: user,
-    token: token
-  }
-}
-
-export const authFailure = errors => {
-  return {
-    type: types.AUTHENTICATION_FAILURE,
-    errors: errors
   }
 }
