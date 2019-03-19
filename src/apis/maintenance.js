9f7767abc112b212bdfa69f23156da7fdc176ceb
BuffaloRob <peckham.rob@gmail.com>
2019-03-19 09:41:37 -0400

    maintenance: changed headers of axios instance

--- src/apis/maintenance.js
+++ src/apis/maintenance.js
@@ -3,5 +3,9 @@ import axios from 'axios';
 // Set-up for the json-api server
 export default axios.create({
   baseURL: 'http://localhost:3000/api/v1',
-  headers: { Authorization: `Bearer ${sessionStorage.jwt}` }
+  headers: { 
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': sessionStorage.jwt
+  }
 })
