a5878e45888f415cfa4f6960bdc852e845ffac8a
BuffaloRob <peckham.rob@gmail.com>
2019-03-22 10:15:18 -0400

    maintenance: changed to work with cookies

--- src/apis/maintenance.js
+++ src/apis/maintenance.js
@@ -3,9 +3,5 @@ import axios from 'axios';
 // Set-up for the json-api server
 export default axios.create({
   baseURL: 'http://localhost:3000/api/v1',
-  headers: { 
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': sessionStorage.jwt
-  }
+  withCredentials: true
 })
