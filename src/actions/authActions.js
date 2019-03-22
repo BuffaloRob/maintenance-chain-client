f4f49068568db489214695ecad413a30dc6a6023
BuffaloRob <peckham.rob@gmail.com>
2019-03-22 10:14:45 -0400

    authActions: + new signup method for cookies

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -3,22 +3,44 @@ import { SET_USER, LOGOUT } from './types';
 
 const API_URL = "http://localhost:3000/api/v1"
 
+// export const signup = (user, callback) => {
+//   let data = {
+//     method: 'POST',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({ user })
+//   }
+
+//   return dispatch => {
+//     fetch(`${API_URL}/signup`, data)
+//       .then(resp => resp.json())
+//       .then(user => {
+//         sessionStorage.setItem('jwt', user.jwt)
+
+//         dispatch({
+//           type: SET_USER,
+//           payload: user.currentUser
+//         })
+
+//         callback();
+//       })
+//       .catch(err => err)
+//   }
+// }
+
 export const signup = (user, callback) => {
   let data = {
     method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ user })
+    credentials: 'include',
+    body: JSON.stringify(user)
   }
 
   return dispatch => {
     fetch(`${API_URL}/signup`, data)
       .then(resp => resp.json())
       .then(user => {
-        sessionStorage.setItem('jwt', user.jwt)
-
         dispatch({
           type: SET_USER,
           payload: user.currentUser
@@ -30,54 +52,54 @@ export const signup = (user, callback) => {
   }
 }
 
-// export const login = (user, callback) => {
-//   let data = {
-//     method: 'POST',
-//     credentials: 'include',
-//     body: JSON.stringify(user)
-//   }
-
-//   return dispatch => {
-//     fetch(`${API_URL}/login`, data)
-//       .then(resp => resp.json())
-//       .then(user => {
-//         dispatch({
-//           type: SET_USER,
-//           payload: user.currentUser
-//         })
-        
-//         callback()
-//       })
-//       .catch(err => err)
-//   } 
-// }
-
 export const login = (user, callback) => {
   let data = {
-    method: "POST",
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ user })
+    method: 'POST',
+    credentials: 'include',
+    body: JSON.stringify(user)
   }
 
   return dispatch => {
     fetch(`${API_URL}/login`, data)
       .then(resp => resp.json())
       .then(user => {
-        sessionStorage.setItem('jwt', user.jwt)
-
         dispatch({
           type: SET_USER,
           payload: user.currentUser
         })
-
-        callback()
+        
+        callback();
       })
       .catch(err => err)
-  }
+  } 
 }
+
+// export const login = (user, callback) => {
+//   let data = {
+//     method: "POST",
+//     headers: {
+//       "Accept": "application/json",
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({ user })
+//   }
+
+//   return dispatch => {
+//     fetch(`${API_URL}/login`, data)
+//       .then(resp => resp.json())
+//       .then(user => {
+//         sessionStorage.setItem('jwt', user.jwt)
+
+//         dispatch({
+//           type: SET_USER,
+//           payload: user.currentUser
+//         })
+
+//         callback()
+//       })
+//       .catch(err => err)
+//   }
+// }
 
 // export const getUser = (credentials) => {
 //   const request = new Request(`${API_URL}/find_user`, {
