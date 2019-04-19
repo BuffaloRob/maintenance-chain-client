ff79ab0b99cb5ff8bb4a37237f8a7bf3b4c3cb41
BuffaloRob <peckham.rob@gmail.com>
2019-04-19 09:48:00 -0400

    LogCreate: + basic component

---
+++ src/components/logs/LogCreate.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { connect } from 'react-redux';
+
+import LogForm from './LogForm';
+import { createLog } from '../../actions/logActions';
+//need to import Connect() and wire up to action creator
+
+class LogCreate extends React.Component {
+
+  onSubmit = formValues => {
+    this.props.createLog(formValues);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create a New Item to Track</h3>
+        <LogForm onSubmit={this.onSubmit} />
+      </div>
+    );
+  }
+}
+
+export default connect(null, { createLog })(LogCreate);
