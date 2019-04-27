3c2ea767d54817d0279875e80bbdc4fd9ac057ef
BuffaloRob <peckham.rob@gmail.com>
2019-04-27 11:21:59 -0400

    itemActions: changed history to redirect to correct address

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -6,7 +6,7 @@ export const createItem = formValues => async (dispatch, getState) => {
   const user_id = getState().auth.currentUser.user_id;
   const response = await apiURL.post('/items', { ...formValues, user_id });
   dispatch({ type: types.CREATE_ITEM, payload: response.data });
-  history.push('/');
+  history.push('/items');
 };
 
 export const fetchItems = () => async dispatch => {
@@ -22,13 +22,13 @@ export const fetchItem = id => async dispatch => {
 export const editItem = (id, formValues) => async dispatch => {
   const response = await apiURL.put(`/items/${id}`, formValues);
   dispatch({ type: types.EDIT_ITEM, payload: response.data });
-  history.push('/');
+  history.push('/items');
 }
 
 export const deleteItem = id => async dispatch => {
   await apiURL.delete(`/items/${id}`);
   dispatch({ type: types.DELETE_ITEM, payload: id });
-  history.push('/');
+  history.push('/items');
 }
 
 
