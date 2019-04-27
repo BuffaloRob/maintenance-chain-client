53faec3ab3ebf9bba076073e8188c089575cfdf0
BuffaloRob <peckham.rob@gmail.com>
2019-04-27 11:21:01 -0400

    app: + home route as root & + spacing for clarification between routes

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import { Router, Route } from 'react-router-dom';
 import Header from './Header';
 import history from '../history';
 
+import Home from './Home';
+
 import ItemList from './items/ItemList';
 import ItemCreate from './items/ItemCreate';
 import ItemDelete from './items/ItemDelete';
@@ -30,18 +32,22 @@ const App = () => {
       <Router history={history}>
         <div>
           <Header />
-          <Route exact path="/" component={ItemList} />
+          <Route exact path="/" component={Home} />  
+          <Route path="/signup" component={SignUp} />
+          <Route path="/login" component={Login} />
+          
+          <Route exact path="/items" component={ItemList} />
           <Route path="/items/delete/:id" component={ItemDelete} />
           <Route exact path="/items/show" component={ItemShow} />
           <Route path="/items/new" component={ItemCreate} />
           <Route path="/items/edit/:id" component={ItemEdit} />
-          <Route path="/signup" component={SignUp} />
-          <Route path="/login" component={Login} />
-          <Route path="/categories/list" component={CategoryList} />
+
+          <Route path="/items/:id/categories/list" component={CategoryList} />
           <Route path="/categories/delete/:id" component={CategoryDelete} />
           <Route exact path="/categories/show" component={CategoryShow} />
           <Route path="/categories/new" component={CategoryCreate} />
           <Route path="/categories/edit/:id" component={CategoryEdit} />
+
           <Route exact path="/logs/list" component={LogList} />
           <Route path="/logs/delete/:id" component={LogDelete} />
           <Route exact path="/logs/show" component={LogShow} />
