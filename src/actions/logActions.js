63da61a5159fca8e7527ec4c00c89a65772795c6
BuffaloRob <peckham.rob@gmail.com>
2019-05-08 13:40:37 -0400

    logActions: basic log actions copied from  category actions

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -2,33 +2,32 @@ import history from '../history';
 import apiURL from '../apis/maintenance';
 import * as types from './types';
 
-export const createLog = formValues => async (dispatch, getState) => {
-  const user_id = getState().auth.currentUser.user_id;
-  const response = await apiURL.post('/logs', { ...formValues, user_id });
+export const createLog = (formValues, itemId) => async dispatch => {
+  const response = await apiURL.post(`/items/${itemId}/logs`, { ...formValues });
   dispatch({ type: types.CREATE_LOG, payload: response.data });
-  history.push('/');
+  history.push(`/items/${itemId}`);
 };
 
-export const fetchLogs = () => async dispatch => {
-  const response = await apiURL.get('/logs');
-  dispatch({ type: types.FETCH_LOGS, payload: response.data });
+export const fetchLogs = itemId => async dispatch => {
+  const response = await apiURL.get(`/items/${itemId}/logs`);
+  dispatch({ type: types.FETCH_CATEGORIES, payload: response.data });
 }
 
-export const fetchLog = id => async dispatch => {
-  const response = await apiURL.get(`/logs/${id}`);
+export const fetchLog = (id, itemId) => async dispatch => {
+  const response = await apiURL.get(`/items/${itemId}/logs/${id}`);
   dispatch({ type: types.FETCH_LOG, payload: response.data });
 }
 
-export const editLog = (id, formValues) => async dispatch => {
-  const response = await apiURL.put(`/logs/${id}`, formValues);
+export const editLog = (formValues, id, itemId) => async dispatch => {
+  const response = await apiURL.put(`/items/${itemId}/logs/${id}`, formValues);
   dispatch({ type: types.EDIT_LOG, payload: response.data });
-  history.push('/');
+  history.push(`/items/${itemId}`);
 }
 
-export const deleteLog = id => async dispatch => {
-  await apiURL.delete(`/logs/${id}`);
+export const deleteLog = (id, itemId) => async dispatch => {
+  await apiURL.delete(`/items/${itemId}/logs/${id}`);
   dispatch({ type: types.DELETE_LOG, payload: id });
-  history.push('/');
+  history.push(`/items/${itemId}`);
 }
 
 
