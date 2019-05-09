cc0bb0ade2ccedd2c83d57a9f00e7121c600a984
BuffaloRob <peckham.rob@gmail.com>
2019-05-09 10:52:06 -0400

    reducer index: removed google

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,11 +5,9 @@ import authReducer from './authReducer';
 import itemReducer from './itemReducer';
 import categoryReducer from './categoryReducer';
 import logReducer from './logReducer';
-import googleReducer from './googleReducer';
 
 export default combineReducers({
   auth: authReducer,
-  google: googleReducer,
   form: formReducer,
   items: itemReducer,
   categories: categoryReducer,
