5ac88547bfafbae12eba1ba8c0f6b65c58d5219d
BuffaloRob <peckham.rob@gmail.com>
2019-05-09 20:12:29 -0400

    CategoryShow: now able to pull category name and number that matches specific route you are on

--- src/components/categories/CategoryShow.js
+++ src/components/categories/CategoryShow.js
@@ -7,14 +7,14 @@ import { fetchLogs } from '../../actions/logActions';
 // Refactor this into the LogList component
 
 class CategoryShow extends React.Component {
-
+  
   componentDidMount() {
-    const itemId = this.props.match.params.id
+    const itemId = this.props.match.params.itemId
     this.props.fetchLogs(itemId)
   }
 
   renderAdmin(log) {
-    const itemId = this.props.match.params.id
+    const itemId = this.props.match.params.itemId
 
     if (this.props.isAuthenticated) {
       return (
@@ -27,15 +27,27 @@ class CategoryShow extends React.Component {
   }
 
   renderList() {
-    const itemId = parseInt(this.props.match.params.id)
+    const itemId = parseInt(this.props.match.params.itemId)
+
+    // catArray returns an array of category objects for that item but only the one that matches the URL catId will have info, the rest will be empty objects
+    const catArray = this.props.categories.map(cat => {
+      if (cat.id === parseInt(this.props.match.params.id)) {
+        return cat
+      }
+    })
+    // filteredArray removes any empty objects from catArray
+    const filteredArray = catArray.filter(cat => cat);
+    // catName & catId pull the info from the only object in filteredArray
+    const catName = filteredArray[0].name;
+    const catId = parseInt(filteredArray[0].id);
 
     return this.props.logs.map(log => {
-      // if (catId === log.cat.id)
-      if (itemId === log.item.id) {
+      if (catId === log.cat.id) {
         return (
           <div className='item' key={log.id} >
             {this.renderAdmin(log)}
             <Link to={`items/${itemId}/categories/${log.id}`} className='content'>{log.name}</Link>
+            {catName}
           </div>
         )
       }
@@ -43,7 +55,7 @@ class CategoryShow extends React.Component {
   }
 
   renderCreate() {
-    const itemId = this.props.match.params.id
+    const itemId = this.props.match.params.itemId
 
     if (this.props.isAuthenticated) {
       return (
@@ -55,9 +67,18 @@ class CategoryShow extends React.Component {
   }
 
   render() {
+    const catArray = this.props.categories.map(cat => {
+      if (cat.id === parseInt(this.props.match.params.id)) {
+        return cat
+      }
+    })
+    const filteredArray = catArray.filter(cat => cat);
+    const catName = filteredArray[0].name;
+
     return (
       <div>
-        <h3>Maintenance Logs for {this.props.item.name}</h3>
+        {/* <h3>Maintenance Logs for {this.props.item.name}</h3> */}
+        <h3>Maintenance Logs for {catName}</h3>
         <div className='ui celled list'>{this.renderList()}</div>
         {this.renderCreate()}
       </div>
