efc994638a1283aab867f7288581f8440196365b
BuffaloRob <peckham.rob@gmail.com>
2019-05-11 15:37:21 -0400

    SignUp: reformatted <Field>s and + type of email

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -16,7 +16,7 @@ class SignUp extends React.Component {
     }
   }
 
-  renderInput = ({ input, label, meta }) => {
+  renderInput = ({ input, label, type, meta }) => {
     const className = `field ${meta.error && meta.touched ? 'error' : ''}`
     return (
       <div className={className}>
@@ -36,8 +36,18 @@ class SignUp extends React.Component {
       // handleSubmit comes from reduxForm
       <form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form error'>
         <h3>Sign Up</h3>
-        <Field name='email' component={this.renderInput} label='Enter Your Email'/>
-        <Field name='password' type='password' component={this.renderInput} label='Enter Your Password'/>
+        <Field 
+          name='email' 
+          type='email'
+          component={this.renderInput} 
+          label='Enter Your Email'
+        />
+        <Field 
+          name='password' 
+          type='password' 
+          component={this.renderInput} 
+          label='Enter Your Password'
+        />
         <button className='ui button primary'>Submit</button>
       </form>
     )
