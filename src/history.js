d6ff7bddc29a4895d9dc25420e6aef7bb31ff1ed
BuffaloRob <peckham.rob@gmail.com>
2019-05-12 13:11:59 -0400

    history: changed import format to clear console warning

--- src/history.js
+++ src/history.js
@@ -1,4 +1,5 @@
 //history package installed with react-router-dom
-import createHistory from "history/createBrowserHistory";
+// import createHistory from "history/createBrowserHistory";
+import { createBrowserHistory } from 'history'
 
-export default createHistory();
+export default createBrowserHistory();
