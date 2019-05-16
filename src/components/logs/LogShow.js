2c5b77d90138aac5452e5fe653e60ccb6bd3b560
BuffaloRob <peckham.rob@gmail.com>
2019-05-15 21:12:53 -0400

    LogShow: formatted render list with icons next to each item, changed renderAdmin Links to be consistant

--- src/components/logs/LogShow.js
+++ src/components/logs/LogShow.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import { connect } from "react-redux";
+import moment from 'moment';
 
 import { fetchItem } from '../../actions/itemActions';
 import { fetchLog } from '../../actions/logActions';
@@ -13,15 +14,15 @@ class LogShow extends React.Component {
     this.props.fetchLog(logId)
   }
 
-  renderAdmin(category) {
+  renderAdmin(log) {
+    const itemId = this.props.match.params.itemId
+    const catId = this.props.match.params.catId
 
-    const itemId = this.props.match.params.id
-    //authenticating differenetly than in ItemList
     if (this.props.isAuthenticated) {
       return (
         <div className='right floated content'>
-          <Link className='ui button primary' to={`/items/${itemId}/categories/edit/${category.id}`}>Edit</Link>
-          <Link className='ui button negative' to={`/items/${itemId}/categories/delete/${category.id}`}>Delete</Link>
+          <Link className='ui button primary' to={`/items/${itemId}/categories/${catId}/logs/edit/${log.id}`}>Edit</Link>
+          <Link className='ui button negative' to={`/items/${itemId}/categories/${catId}/logs/delete/${log.id}`}>Delete</Link>
         </div>
       )
     }
@@ -29,29 +30,60 @@ class LogShow extends React.Component {
 
   renderList() {
     const logId = parseInt(this.props.match.params.id)
+    const log = this.props.log
+    const formattedDatePerformed = moment(log.date_performed).format("MMM Do YYYY");
+    const formattedDateDue = moment(log.date_due).format("MMM Do YYYY");
 
-    return this.props.categories.map(log => {
-      // change if statement to make sense
-      if (logId === log.id) {
-        return (
-          <div className='item' key={log.id} >
-            {this.renderAdmin(log)}
-            <li>{log.date_due}</li>
-            <li>{log.date_performed}</li>
-            <li>{log.cost}</li>
-            <li>{log.tools}</li>
-            <li>{log.notes}</li>
+    if (logId === log.id) {
+      return (
+        <div className='ui celled grid' key={log.id} >
+          <div className='row'>
+            <div className='three wide column'>
+              <i class="hourglass start icon" />
+              Performed On:
+            </div>
+            <div className='thirteen wide column'>{formattedDatePerformed}</div>
+          </div> 
+          <div className='row'>
+            <div className='three wide column'>
+              <i class="hourglass end icon" />
+              Due On:
+            </div>
+            <div className='thirteen wide column'>{formattedDateDue}</div>
           </div>
-        )
-      }
-    });
+          <div className='row'>
+            <div className='three wide column'>
+              <i class="dollar sign icon" />
+              Cost:
+            </div>
+            <div className='thirteen wide column'>$ {log.cost}</div>
+          </div>
+          <div className='row'>
+            <div className='three wide column'>
+              <i className='wrench icon' />
+              Tools Used:
+            </div>
+            <div className='thirteen wide column'>{log.tools}</div>
+          </div>
+          <div className='row'>
+            <div className='three wide column'>
+              <i className='pencil icon' />
+              Notes:
+            </div>
+            <div className='thirteen wide column'>{log.notes}</div>
+          </div>
+
+        </div>
+      )
+    };
   }
 
   render() {
     return (
       <div>
-        <h3>Maintenance Categories for {this.props.item.name}</h3>
-        <div className='ui celled list'>{this.renderList()}</div>
+        <h2 className='ui header'>Maintenance Log</h2>
+        <div>{this.renderList()}</div>
+        <div>{this.renderAdmin(this.props.log)}</div>
       </div>
     )
   }
@@ -59,12 +91,10 @@ class LogShow extends React.Component {
 
 const mapStateToProps = (state, ownProps) => {
   return ({
-    item: state.items[ownProps.match.params.id],
-    categories: Object.values(state.categories),
+    item: state.items[ownProps.match.params.itemId],
+    log: state.logs[ownProps.match.params.id],
     isAuthenticated: state.auth.isAuthenticated
   })
 }
 
 export default connect(mapStateToProps, { fetchItem, fetchLog })(LogShow);
-
-// get item :id from url params and use that to make list of categories for that item
