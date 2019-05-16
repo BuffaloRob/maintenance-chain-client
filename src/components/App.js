94eb8eaa728597a48f2c4edccaacb9325f4cc0f2
BuffaloRob <peckham.rob@gmail.com>
2019-05-15 21:10:50 -0400

    App: reconfigured routes to working setup, shortened path for LogShow

--- src/components/App.js
+++ src/components/App.js
@@ -19,9 +19,7 @@ import CategoryCreate from './categories/CategoryCreate';
 import CategoryDelete from './categories/CategoryDelete';
 import CategoryEdit from './categories/CategoryEdit';
 import CategoryShow from './categories/CategoryShow';
-import CategoryList from './categories/CategoryList';
 
-// import LogList from './logs/LogList';
 import LogCreate from './logs/LogCreate';
 // import LogDelete from './logs/LogDelete';
 // import LogEdit from './logs/LogEdit';
@@ -33,29 +31,25 @@ const App = () => {
         <>
           <Header />
           <Switch>
-            <Route path="/items/:itemId/categories/:id/logs/new" component={LogCreate} />
-            {/* <Route exact path="/logs/list" component={LogList} /> */}
-            {/* <Route path="/logs/delete/:id" component={LogDelete} /> */}
-            {/* <Route path="/logs/edit/:id" component={LogEdit} />    */}
-            <Route exact path="items/:itemId/categories/:catId/logs/:id" component={LogShow} />
-
+            <Route exact path="/items/:itemId/categories/:id/logs/new" component={LogCreate} />
+            <Route path="/logs/:id" component={LogShow} />
+            {/* <Route path="items/:itemId/categories/:catId/logs/delete/:id" component={LogDelete} /> */}
+            {/* <Route path="items/:itemId/categories/:catId/logs/edit/:id" component={LogEdit} />    */}
+            
             <Route exact path="/items/:id/categories/new" component={CategoryCreate} />
-            {/* <Route exact path="/items/:id/categories/list" component={CategoryList} /> */}
-            <Route path="/items/:itemId/categories/:id" component={CategoryShow} />
+            <Route exact path="/items/:itemId/categories/:id" component={CategoryShow} />
             <Route path="/items/:itemId/categories/delete/:id" component={CategoryDelete} />
             <Route path="/items/:itemId/categories/edit/:id" component={CategoryEdit} />
-            
-            
-            <Route exact path="/" component={Home} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/login" component={Login} />
 
             <Route exact path="/items" component={ItemList} />
-            <Route exact path="/items/new" component={ItemCreate} />
+            <Route path="/items/new" component={ItemCreate} />
             <Route path="/items/delete/:id" component={ItemDelete} />
             <Route path="/items/edit/:id" component={ItemEdit} />
-            <Route path="/items/:id" component={ItemShow} />
-            
+            <Route exact path="/items/:id" component={ItemShow} />
+
+            <Route exact path="/" component={Home} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/login" component={Login} />  
           </Switch>
          
         </>
