063fd9044cca3f4b1ed6a59128b4d7e3b86ad462
BuffaloRob <peckham.rob@gmail.com>
2019-05-16 12:07:43 -0400

    index: configured persist to work

--- src/index.js
+++ src/index.js
@@ -3,19 +3,37 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import reduxThunk from "redux-thunk";
+import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from "./components/App";
-import reducers from "./reducers";
+// import reducers from "./reducers";
+import rootReducer from './reducers'
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// }
+
+// const persistedReducer = persistReducer(persistConfig, reducers)
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  reducers,
+  rootReducer,
   composeEnhancers(applyMiddleware(reduxThunk))
 );
+export const persistor = persistStore(store);
+
+// uncomment to clear store
+// persistor.purge();
+
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.querySelector("#root")
 );
