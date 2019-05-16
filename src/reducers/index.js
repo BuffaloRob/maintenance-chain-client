456f87042e0e9194f29d3e6aed4cff3fc82c376f
BuffaloRob <peckham.rob@gmail.com>
2019-05-16 12:09:01 -0400

    reducer index: using persist and clearing state when logging out via CLEAR_DATA

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,41 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import { persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/lib/storage/session';
 
 import authReducer from './authReducer';
 import itemReducer from './itemReducer';
 import categoryReducer from './categoryReducer';
 import logReducer from './logReducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const appReducer = persistCombineReducers(persistConfig, {
   auth: authReducer,
   form: formReducer,
   items: itemReducer,
   categories: categoryReducer,
   logs: logReducer
-});
+})
+
+const rootReducer = (state, action) => {
+  if (action.type === 'CLEAR_DATA') {
+    storage.removeItem('persist:root')
+    state = undefined
+  }
+
+  return appReducer(state, action)
+}
+
+export default rootReducer
+
+// export default combineReducers({
+//   auth: authReducer,
+//   form: formReducer,
+//   items: itemReducer,
+//   categories: categoryReducer,
+//   logs: logReducer
+// });
