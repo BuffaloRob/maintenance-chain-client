2c7285c3a864d976758c1370e82bd449e1d6027a
BuffaloRob <peckham.rob@gmail.com>
2019-05-22 15:30:45 -0400

    ItemShow: commented out old code to prep for refactor

--- src/components/items/ItemShow.js
+++ src/components/items/ItemShow.js
@@ -1,79 +1,114 @@
-import React from "react";
-import { Link } from 'react-router-dom';
-import { connect } from "react-redux";
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
 
-import { fetchItem } from '../../actions/itemActions';
-import { fetchCategories } from '../../actions/categoryActions';
+const ItemShow = ({ items }) => {
+  // const renderList = Object.keys(items).map(item => {
+  //   const itemId = parseInt(this.props.match.params.id)
 
-//Refactor this into the CategoryList component
-class ItemShow extends React.Component {
+  //   return items[item].categories.map(cat => {
+  //     if (itemId === cat.item.id) {
+  //       return (
+  //         <div className='item' key={cat.id} >
+  //           {this.renderAdmin(cat)}
+  //           <i className='large middle aligned icon wrench' />
+  //           <Link to={`/items/${itemId}/categories/${cat.id}`} className='content'>{cat.name}</Link>
+  //         </div>
+  //       )
+  //     }
+  //   });
+  // })   
 
-  componentDidMount() {
-    const itemId = this.props.match.params.id
-    this.props.fetchCategories(itemId)
-  }
+  return (
+    <div>
+      <h3>Maintenance Categories for </h3>
+      {/* <h3>Maintenance Categories for {this.props.item.name}</h3> */}
+      {/* <div className='ui celled list'>{renderList}</div> */}
+       {/* {this.renderCreate()} */}
+    </div>
+  )
 
-  renderAdmin(category) {
+}
+
+export default ItemShow;
+
+
+
+// import React from "react";
+// import { Link } from 'react-router-dom';
+// import { connect } from "react-redux";
+
+// import { fetchItem } from '../../actions/itemActions';
+// import { fetchCategories } from '../../actions/categoryActions';
+
+// //Refactor this into the CategoryList component
+// class ItemShow extends React.Component {
+
+//   componentDidMount() {
+//     const itemId = this.props.match.params.id
+//     this.props.fetchCategories(itemId)
+//   }
+
+//   renderAdmin(category) {
    
-    const itemId = this.props.match.params.id
-    //authenticating differenetly than in ItemList
-    if (this.props.isAuthenticated) {
-      return (
-        <div className='right floated content'>
-          <Link className='ui button primary' to={`/items/${itemId}/categories/edit/${category.id}`}>Edit</Link>
-          <Link className='ui button negative' to={`/items/${itemId}/categories/delete/${category.id}`}>Delete</Link>
-        </div>
-      )
-    }
-  }
+//     const itemId = this.props.match.params.id
+//     //authenticating differenetly than in ItemList
+//     if (this.props.isAuthenticated) {
+//       return (
+//         <div className='right floated content'>
+//           <Link className='ui button primary' to={`/items/${itemId}/categories/edit/${category.id}`}>Edit</Link>
+//           <Link className='ui button negative' to={`/items/${itemId}/categories/delete/${category.id}`}>Delete</Link>
+//         </div>
+//       )
+//     }
+//   }
 
-  renderList() {
-    const itemId = parseInt(this.props.match.params.id)
+  // renderList() {
+  //   const itemId = parseInt(this.props.match.params.id)
 
-    return this.props.categories.map(cat => {
-      if (itemId === cat.item.id) {
-        return (
-          <div className='item' key={cat.id} >
-            {this.renderAdmin(cat)}
-            <i className='large middle aligned icon wrench' />
-            <Link to={`/items/${itemId}/categories/${cat.id}`} className='content'>{cat.name}</Link>
-          </div>
-        )
-      } 
-    });
-  }
+  //   return this.props.categories.map(cat => {
+  //     if (itemId === cat.item.id) {
+  //       return (
+  //         <div className='item' key={cat.id} >
+  //           {this.renderAdmin(cat)}
+  //           <i className='large middle aligned icon wrench' />
+  //           <Link to={`/items/${itemId}/categories/${cat.id}`} className='content'>{cat.name}</Link>
+  //         </div>
+  //       )
+  //     } 
+  //   });
+  // }
 
-  renderCreate() {
-    const itemId = this.props.match.params.id
+//   renderCreate() {
+//     const itemId = this.props.match.params.id
 
-    if (this.props.isAuthenticated) {
-      return (
-        <div style={{ textAlign: 'right' }} >
-          <Link to={`/items/${itemId}/categories/new`} className='ui button primary'>Create New Category</Link>
-        </div>
-      )
-    }
-  }
+//     if (this.props.isAuthenticated) {
+//       return (
+//         <div style={{ textAlign: 'right' }} >
+//           <Link to={`/items/${itemId}/categories/new`} className='ui button primary'>Create New Category</Link>
+//         </div>
+//       )
+//     }
+//   }
 
-  render() {
-    return (
-      <div>
-        <h3>Maintenance Categories for {this.props.item.name}</h3>
-        <div className='ui celled list'>{this.renderList()}</div>
-        {this.renderCreate()}
-      </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <h3>Maintenance Categories for {this.props.item.name}</h3>
+//         <div className='ui celled list'>{this.renderList()}</div>
+//         {this.renderCreate()}
+//       </div>
+//     )
+//   }
+// }
 
-const mapStateToProps = (state, ownProps) => {
-  return ({
-    item: state.items[ownProps.match.params.id],
-    categories: Object.values(state.categories),
-    isAuthenticated: state.auth.isAuthenticated
-  })
-}
+// const mapStateToProps = (state, ownProps) => {
+//   return ({
+//     item: state.items[ownProps.match.params.id],
+//     categories: Object.values(state.categories),
+//     isAuthenticated: state.auth.isAuthenticated
+//   })
+// }
 
-export default connect(mapStateToProps, { fetchItem, fetchCategories })(ItemShow);
+// export default connect(mapStateToProps, { fetchItem, fetchCategories })(ItemShow);
 
-// get item :id from url params and use that to make list of categories for that item
+// // get item :id from url params and use that to make list of categories for that item
