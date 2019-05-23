d004766e6d4c4eae228381d6e51d68c3b162338e
BuffaloRob <peckham.rob@gmail.com>
2019-05-23 11:05:39 -0400

    Item: replaced triggerSelectItem with arrow func in the onClick

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Item = ({ item, selectItem }) => {
-  const triggerSelectItem = event => {
-    event.preventDefault();
-    selectItem(event.target.id)
-  }
+  // const triggerSelectItem = event => {
+  //   event.preventDefault();
+  //   selectItem(event.target.id)
+  // }
   return (
-    <div className='item' key={item.id} id={item.id} onClick={triggerSelectItem}>
+    // <div className='item' key={item.id} id={item.id} onClick={triggerSelectItem}>
+    <div className='item' key={item.id} id={item.id} onClick={() => selectItem(item.id)}>
+    {/* <div className='item' key={item.id} id={item.id} > */}
       <i className="large middle aligned wrench icon" />
       <h3 className='content' id={item.id} >{item.name}</h3>
     </div>
