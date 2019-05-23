3772c3866f9db411b8bbe83985568e464f2046d3
BuffaloRob <peckham.rob@gmail.com>
2019-05-23 15:09:28 -0400

    selectCategoryReducer: basic reducer with alternative commented out

---
+++ src/reducers/selectedCategoryReducer.js
@@ -0,0 +1,11 @@
+import { SELECT_CATEGORY } from '../actions/types';
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case SELECT_CATEGORY:
+      // return Object.assign({}, state, action.payload)
+      return { ...state, [action.payload.id]: action.payload };
+    default:
+      return state
+  }
+}
