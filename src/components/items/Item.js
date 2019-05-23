853384204a1c4d2b7f811c54ff88276b136ddf95
BuffaloRob <peckham.rob@gmail.com>
2019-05-23 15:13:06 -0400

    Item: removed unneeded code

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -1,14 +1,8 @@
 import React from 'react';
 
 const Item = ({ item, selectItem }) => {
-  // const triggerSelectItem = event => {
-  //   event.preventDefault();
-  //   selectItem(event.target.id)
-  // }
   return (
-    // <div className='item' key={item.id} id={item.id} onClick={triggerSelectItem}>
     <div className='item' key={item.id} id={item.id} onClick={() => selectItem(item.id)}>
-    {/* <div className='item' key={item.id} id={item.id} > */}
       <i className="large middle aligned wrench icon" />
       <h3 className='content' id={item.id} >{item.name}</h3>
     </div>
