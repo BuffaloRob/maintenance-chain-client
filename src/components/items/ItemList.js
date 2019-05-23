c3cb172f44f3c3117e86da872773417467ecf975
BuffaloRob <peckham.rob@gmail.com>
2019-05-23 15:12:16 -0400

    ItemList: cleaned up and passing key prop to <Item>

--- src/components/items/ItemList.js
+++ src/components/items/ItemList.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
+
 import ItemShow from './ItemShow';
 import Item from './Item';
+import { fetchItems } from '../../actions/itemActions';
 
 const ItemList = ({ items, selectItem }) => {
+ 
   const renderList = Object.keys(items).map(item => (
     <Item
+      key={item.id}
       item={items[item]}
       selectItem={selectItem}
     />
@@ -22,6 +26,10 @@ const ItemList = ({ items, selectItem }) => {
       </div>
     </div>
   )
+}
+
+export default ItemList;
+
 // const ItemList = ({ items }) => {
 //   // debugger
 //   const renderList = Object.keys(items).map(item => {
@@ -64,78 +72,3 @@ const ItemList = ({ items, selectItem }) => {
 //       )
 //     // }
 //   }
-
-}
-
-export default ItemList;
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import { connect } from 'react-redux';
-
-// import { fetchItems } from '../../actions/itemActions';
-// import { fetchUser } from '../../actions/authActions';
-
-// class ItemList extends React.Component {
-
-//   componentDidMount() {
-//     // this.props.fetchUser();
-//     this.props.fetchItems();
-//   }
-
-  // renderAdmin(item) {
-  //   // if (item.user.id === this.props.currentUserId) {
-  //   if (this.props.isAuthenticated) {
-  //     return (
-  //       <div className='right floated content'>
-  //         <Link className='ui button primary' to={`/items/edit/${item.id}`}>Edit</Link>
-  //         <Link className='ui button negative' to={`/items/delete/${item.id}`}>Delete</Link>
-  //       </div>
-  //     )
-  //   }
-  // }
-
-  // renderList() {
-    // return this.props.items.map(item => {
-    //   return ( 
-    //     <div className="item" key={item.id}>
-    //       {this.renderAdmin(item)}
-    //       <i className="large middle aligned icon wrench" />
-    //       <Link to={ `items/${item.id}` } className='content'>{item.name}</Link>
-    //     </div>
-    //   )
-    // });
-  // }
- 
-  // renderCreate() {
-  //   if (this.props.isAuthenticated) {
-  //     return (
-  //       <div style={{ textAlign: 'right' }}>
-  //         <Link to="/items/new" className='ui button primary'>
-  //           Create New Item
-  //         </Link>
-  //       </div>
-  //     )
-  //   }
-  // }
-
-//   render() {
-    // return (
-    //   <div>
-    //     <h2 className='ui header'>Items</h2>
-    //     <div className='ui celled list'>{this.renderList()}</div>
-    //     {this.renderCreate()}
-    //   </div>
-    // )
-//   }
-// }
-
-// const mapStateToProps = (state) => {
-//   return ({ 
-//     items: Object.values(state.items),
-//     currentUserId: state.auth.currentUser.user_id,
-//     isAuthenticated: state.auth.isAuthenticated,
-//   });
-// }
-
-// export default connect(mapStateToProps, { fetchItems, fetchUser })(ItemList);
