d8991926eb5576350b96a9c9d1eea4b50c48ab1a
BuffaloRob <peckham.rob@gmail.com>
2019-05-23 15:17:12 -0400

    selectActions: + selectCategory()

--- src/actions/selectActions.js
+++ src/actions/selectActions.js
@@ -1,8 +1,13 @@
 import history from '../history';
-import { SELECT_ITEM } from './types';
+import { SELECT_ITEM, SELECT_CATEGORY } from './types';
 
 export const selectItem = (item) => dispatch => {
-  debugger
   dispatch({ type: SELECT_ITEM, payload: item})
   history.push(`/items/${item.id}`)
+}
+
+export const selectCategory = (cat) => dispatch => {
+  debugger
+  dispatch({ type: SELECT_CATEGORY, payload: cat })
+  history.push(`/categories/${cat.id}`)
 }
