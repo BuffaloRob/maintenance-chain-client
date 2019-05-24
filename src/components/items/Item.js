8175868aa7fe2c01620d4e0d8b8ee4fd2bbf10e9
BuffaloRob <peckham.rob@gmail.com>
2019-05-23 21:53:36 -0400

    Item: + renderAdmin()

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Item = ({ item, selectItem }) => {
+
+  const renderAdmin = item => (
+    <div className='right floated content'>
+      <Link className='ui button primary' to={`/items/edit/${item.id}`}>Edit</Link>
+      <Link className='ui button negative' to={`/items/delete/${item.id}`}>Delete</Link>
+    </div>
+  );
+  
   return (
-    <div className='item' key={item.id} id={item.id} onClick={() => selectItem(item.id)}>
+    <div className='item' key={item.id} >
+      {renderAdmin(item)}
       <i className="large middle aligned wrench icon" />
-      <h3 className='content' id={item.id} >{item.name}</h3>
+        <h3 className='content' id={item.id} onClick={() => selectItem(item.id)}>{item.name}</h3>
     </div>
   )
 }
