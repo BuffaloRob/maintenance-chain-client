ba251f83f594d6dea147f63732b59b544962b36d
BuffaloRob <peckham.rob@gmail.com>
2019-05-24 17:57:51 -0400

    selectActions: moved all history.push to respective methods in <App>

--- src/actions/selectActions.js
+++ src/actions/selectActions.js
@@ -3,15 +3,12 @@ import { SELECT_ITEM, SELECT_CATEGORY, SELECT_LOG } from './types';
 
 export const selectItem = item => dispatch => {
   dispatch({ type: SELECT_ITEM, payload: item })
-  history.push(`/item/${item.id}`)
 }
 
 export const selectCategory = (cat, itemId) => dispatch => {
-  dispatch({ type: SELECT_CATEGORY, payload: cat })
-  history.push(`/item/${itemId}/category/${cat.id}`)
+  dispatch({ type: SELECT_CATEGORY, payload: cat })  
 }
 
 export const selectLog = (log, itemId) => dispatch => {
-  dispatch({ type: SELECT_LOG, payload: log })
-  history.push(`/log/${log.id}`)
+  dispatch({ type: SELECT_LOG, payload: log }) 
 }
