eec3050fee27f144d97cfa8c3e0cfae8fd9450ab
BuffaloRob <peckham.rob@gmail.com>
2019-05-24 10:36:39 -0400

    Log: now receives catId from parent and passes to onClick

--- src/components/logs/Log.js
+++ src/components/logs/Log.js
@@ -2,11 +2,12 @@ import React from 'react';
 import moment from 'moment';
 
 
-const Log = ({ log, selectLog, match }) => {
-  const itemId = match.params.id
+const Log = ({ log, selectLog, match, itemId, categoryId }) => {
+
   const formattedDate = moment(log.date_performed).format("MMM Do YYYY");
+  
   return (
-    <div className='item' key={log.id} id={log.id} onClick={() => selectLog(log.id, itemId)} >
+    <div className='item' key={log.id} id={log.id} onClick={() => selectLog(log.id, itemId, categoryId)} >
       <i className='large middle aligned icon wrench' />
       <h3 className='content' id={log.id}>{formattedDate}</h3>
     </div>
