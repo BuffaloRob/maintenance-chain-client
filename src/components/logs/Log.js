e45b543b8a7b7e9e651bc2aa29d2e6bac1f13b5a
BuffaloRob <peckham.rob@gmail.com>
2019-05-23 21:28:53 -0400

    Log: using Moment to formate date

--- src/components/logs/Log.js
+++ src/components/logs/Log.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import moment from 'moment';
+
 
 const Log = ({ log, selectLog, match }) => {
   const itemId = match.params.id
+  const formattedDate = moment(log.date_performed).format("MMM Do YYYY");
   return (
     <div className='item' key={log.id} id={log.id} onClick={() => selectLog(log.id, itemId)} >
       <i className='large middle aligned icon wrench' />
-      <h3 className='content' id={log.id}>{log.date_performed}</h3>
+      <h3 className='content' id={log.id}>{formattedDate}</h3>
     </div>
   )
 }
