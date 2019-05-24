ffa9912d4afe0cbb61d5dfd2349f1af487db8ed6
BuffaloRob <peckham.rob@gmail.com>
2019-05-24 10:35:24 -0400

    LogShow: renders all content, need to add admin & create buttons

--- src/components/logs/LogShow.js
+++ src/components/logs/LogShow.js
@@ -7,94 +7,148 @@ import { fetchItem } from '../../actions/itemActions';
 import { fetchLog } from '../../actions/logActions';
 
 //Refactor this into the CategoryList component
-class LogShow extends React.Component {
 
-  componentDidMount() {
-    const logId = this.props.match.params.id
-    this.props.fetchLog(logId)
-  }
+const LogShow = ({ log }) => {
+  debugger
+  const formattedDatePerformed = moment(log.date_performed).format("MMM Do YYYY");
+  const formattedDateDue = moment(log.date_due).format("MMM Do YYYY");
 
-  renderAdmin(log) {
-    const itemId = this.props.match.params.itemId
-    const catId = this.props.match.params.catId
+  return (
+    <div>
+      <h2 className='ui header'>Maintenance Log</h2>
 
-    if (this.props.isAuthenticated) {
-      return (
-        <div className='right floated content'>
-          <Link className='ui button primary' to={`/items/${itemId}/logs/edit/${log.id}`}>Edit</Link>
-          <Link className='ui button negative' to={`/items/${itemId}/logs/delete/${log.id}`}>Delete</Link>
+      <div className='ui celled grid' key={log.id} >
+        <div className='row'>
+          <div className='five wide column aligned middle'>
+            <i className="hourglass start icon" />
+            Performed On:
+            </div>
+          <div className='eleven wide column'>{formattedDatePerformed}</div>
         </div>
-      )
-    }
-  }
-
-  renderList() {
-    const logId = parseInt(this.props.match.params.id)
-    const log = this.props.log
-    const formattedDatePerformed = moment(log.date_performed).format("MMM Do YYYY");
-    const formattedDateDue = moment(log.date_due).format("MMM Do YYYY");
-
-    if (logId === log.id) {
-      return (
-        <div className='ui celled grid' key={log.id} >
-          <div className='row'>
-            <div className='five wide column aligned middle'>
-              <i className="hourglass start icon" />
-              Performed On:
+        <div className='row'>
+          <div className='five wide column'>
+            <i className="hourglass end icon" />
+            Due On:
             </div>
-            <div className='eleven wide column'>{formattedDatePerformed}</div>
-          </div> 
-          <div className='row'>
-            <div className='five wide column'>
-              <i className="hourglass end icon" />
-              Due On:
-            </div>
-            <div className='eleven wide column'>{formattedDateDue}</div>
-          </div>
-          <div className='row'>
-            <div className='five wide column'>
-              <i className="dollar sign icon" />
-              Cost:
-            </div>
-            <div className='eleven wide column'>$ {log.cost}</div>
-          </div>
-          <div className='row'>
-            <div className='five wide column'>
-              <i className='wrench icon' />
-              Tools Used:
-            </div>
-            <div className='eleven wide column'>{log.tools}</div>
-          </div>
-          <div className='row'>
-            <div className='five wide column'>
-              <i className='pencil icon' />
-              Notes:
-            </div>
-            <div className='eleven wide column'>{log.notes}</div>
-          </div>
-
+          <div className='eleven wide column'>{formattedDateDue}</div>
         </div>
-      )
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <h2 className='ui header'>Maintenance Log</h2>
-        <div>{this.renderList()}</div>
-        <div>{this.renderAdmin(this.props.log)}</div>
+        <div className='row'>
+          <div className='five wide column'>
+            <i className="dollar sign icon" />
+            Cost:
+            </div>
+          <div className='eleven wide column'>$ {log.cost}</div>
+        </div>
+        <div className='row'>
+          <div className='five wide column'>
+            <i className='wrench icon' />
+            Tools Used:
+            </div>
+          <div className='eleven wide column'>{log.tools}</div>
+        </div>
+        <div className='row'>
+          <div className='five wide column'>
+            <i className='pencil icon' />
+            Notes:
+            </div>
+          <div className='eleven wide column'>{log.notes}</div>
+        </div>
       </div>
+
+    </div>
     )
-  }
+
 }
 
-const mapStateToProps = (state, ownProps) => {
-  return ({
-    item: state.items[ownProps.match.params.itemId],
-    log: state.logs[ownProps.match.params.id],
-    isAuthenticated: state.auth.isAuthenticated
-  })
-}
+export default LogShow;
+// class LogShow extends React.Component {
 
-export default connect(mapStateToProps, { fetchItem, fetchLog })(LogShow);
+//   componentDidMount() {
+//     const logId = this.props.match.params.id
+//     this.props.fetchLog(logId)
+//   }
+
+//   renderAdmin(log) {
+//     const itemId = this.props.match.params.itemId
+//     const catId = this.props.match.params.catId
+
+//     if (this.props.isAuthenticated) {
+//       return (
+//         <div className='right floated content'>
+//           <Link className='ui button primary' to={`/items/${itemId}/logs/edit/${log.id}`}>Edit</Link>
+//           <Link className='ui button negative' to={`/items/${itemId}/logs/delete/${log.id}`}>Delete</Link>
+//         </div>
+//       )
+//     }
+//   }
+
+//   renderList() {
+//     const logId = parseInt(this.props.match.params.id)
+//     const log = this.props.log
+//     const formattedDatePerformed = moment(log.date_performed).format("MMM Do YYYY");
+//     const formattedDateDue = moment(log.date_due).format("MMM Do YYYY");
+
+//     if (logId === log.id) {
+//       return (
+//         <div className='ui celled grid' key={log.id} >
+//           <div className='row'>
+//             <div className='five wide column aligned middle'>
+//               <i className="hourglass start icon" />
+//               Performed On:
+//             </div>
+//             <div className='eleven wide column'>{formattedDatePerformed}</div>
+//           </div> 
+//           <div className='row'>
+//             <div className='five wide column'>
+//               <i className="hourglass end icon" />
+//               Due On:
+//             </div>
+//             <div className='eleven wide column'>{formattedDateDue}</div>
+//           </div>
+//           <div className='row'>
+//             <div className='five wide column'>
+//               <i className="dollar sign icon" />
+//               Cost:
+//             </div>
+//             <div className='eleven wide column'>$ {log.cost}</div>
+//           </div>
+//           <div className='row'>
+//             <div className='five wide column'>
+//               <i className='wrench icon' />
+//               Tools Used:
+//             </div>
+//             <div className='eleven wide column'>{log.tools}</div>
+//           </div>
+//           <div className='row'>
+//             <div className='five wide column'>
+//               <i className='pencil icon' />
+//               Notes:
+//             </div>
+//             <div className='eleven wide column'>{log.notes}</div>
+//           </div>
+
+//         </div>
+//       )
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div>
+        // <h2 className='ui header'>Maintenance Log</h2>
+//         <div>{this.renderList()}</div>
+//         <div>{this.renderAdmin(this.props.log)}</div>
+//       </div>
+//     )
+//   }
+// }
+
+// const mapStateToProps = (state, ownProps) => {
+//   return ({
+//     item: state.items[ownProps.match.params.itemId],
+//     log: state.logs[ownProps.match.params.id],
+//     isAuthenticated: state.auth.isAuthenticated
+//   })
+// }
+
+// export default connect(mapStateToProps, { fetchItem, fetchLog })(LogShow);
