fc51370477c072ad73581d2a1edf40fee46fc5e1
BuffaloRob <peckham.rob@gmail.com>
2019-05-24 17:53:37 -0400

    Item: changed Link routes to match App

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import history from '../../history';
+import { Route } from 'react-router-dom';
+import ItemEdit from './ItemEdit';
 
-const Item = ({ item, selectItem }) => {
+const Item = ({ item, selectItem, editItemClick }) => {
 
   const renderAdmin = item => (
+
     <div className='right floated content'>
-      <Link className='ui button primary' to={`/items/edit/${item.id}`}>Edit</Link>
-      <Link className='ui button negative' to={`/items/delete/${item.id}`}>Delete</Link>
+      <Link className='ui button primary' to={`/item/${item.id}/edit`}>Edit</Link>
+      <Link className='ui button negative' to={`/item/${item.id}/delete`}>Delete</Link>
     </div>
+
   );
   
+  
   return (
+    
     <div className='item' key={item.id} >
       {renderAdmin(item)}
       <i className="large middle aligned wrench icon" />
