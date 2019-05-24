5285e4739470e0a3644f0bac52cde54301f28ee8
BuffaloRob <peckham.rob@gmail.com>
2019-05-24 17:55:16 -0400

    Category: + renderAdmin() & moved onClick to the <h3>

--- src/components/categories/Category.js
+++ src/components/categories/Category.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Category = ({ category, selectCategory, match }) => {
-  const itemId = match.params.id
+const Category = ({ category, selectCategory, match, itemId }) => {
+
+  const renderAdmin = item => (
+    <div className='right floated content'>
+      <Link className='ui button primary' to={`/item/${item.id}/category/${category.id}/edit`}>Edit</Link>
+      <Link className='ui button negative' to={`/item/${item.id}/category/${category.id}/delete`}>Delete</Link>
+    </div>
+  );
+
   return (
-    <div className='item' key={category.id} id={category.id} onClick={() => selectCategory(category.id, itemId)}>
+    <div className='item' key={category.id} >
+      {renderAdmin(category)}
       <i className="large middle aligned wrench icon" />
-      <h3 className='content' id={category.id} >{category.name}</h3>
+      <h3 className='content' id={category.id} onClick={() => selectCategory(category.id, itemId)}>{category.name}</h3>
     </div>
   )
 }
