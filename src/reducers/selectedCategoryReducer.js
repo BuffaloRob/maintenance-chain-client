3e82e142abad5e19e0e48f1bbfe32514ce09b6ea
BuffaloRob <peckham.rob@gmail.com>
2019-05-23 21:28:04 -0400

    selectCategories: using Object.assign to store in state in desired format

--- src/reducers/selectedCategoryReducer.js
+++ src/reducers/selectedCategoryReducer.js
@@ -3,8 +3,7 @@ import { SELECT_CATEGORY } from '../actions/types';
 export default (state = {}, action) => {
   switch (action.type) {
     case SELECT_CATEGORY:
-      // return Object.assign({}, state, action.payload)
-      return { ...state, [action.payload.id]: action.payload };
+      return Object.assign({}, state, action.payload)
     default:
       return state
   }
