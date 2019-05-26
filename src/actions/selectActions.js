42ea489a7861a09163b0f1f4cb297313c5ea22b3
BuffaloRob <peckham.rob@gmail.com>
2019-05-25 22:33:59 -0400

    selectActions: changed all func names for clarity

--- src/actions/selectActions.js
+++ src/actions/selectActions.js
@@ -1,14 +1,14 @@
 import history from '../history';
 import { SELECT_ITEM, SELECT_CATEGORY, SELECT_LOG } from './types';
 
-export const selectItem = item => dispatch => {
+export const itemSelector = item => dispatch => {
   dispatch({ type: SELECT_ITEM, payload: item })
 }
 
-export const selectCategory = (cat, itemId) => dispatch => {
+export const categorySelector = (cat, itemId) => dispatch => {
   dispatch({ type: SELECT_CATEGORY, payload: cat })  
 }
 
-export const selectLog = (log, itemId) => dispatch => {
+export const logSelector = (log, itemId) => dispatch => {
   dispatch({ type: SELECT_LOG, payload: log }) 
 }
