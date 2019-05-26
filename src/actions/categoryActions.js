793ec86f23866f86ad35975482d458b879c486d2
BuffaloRob <peckham.rob@gmail.com>
2019-05-26 10:32:41 -0400

    categoryActions: updated comments

--- src/actions/categoryActions.js
+++ src/actions/categoryActions.js
@@ -7,7 +7,7 @@ import { fetchItems } from '../actions/itemActions';
 
 export const createCategory = (formValues, itemId) => async dispatch => {
   const response = await apiURL.post(`/items/${itemId}/categories`, { ...formValues});
-  // dispatching fetchItems to update state to display correctly in the ItemShow route
+  // dispatching fetchItems to update state to display correctly in the CategoryList route
   dispatch(fetchItems());
   dispatch({ type: types.CREATE_CATEGORY, payload: response.data });  
   history.push(`/items/${itemId}`);
@@ -29,12 +29,11 @@ export const editCategory = (formValues, id, itemId) => async dispatch => {
   dispatch({ type: types.EDIT_CATEGORY, payload: response.data });
   // dispatch(categorySelector(response.data))
   history.push(`/items/${itemId}`);
-
 }
 
 export const deleteCategory = (id, itemId) => async dispatch => {
   await apiURL.delete(`/items/${itemId}/categories/${id}`);
-  // dispatching fetchItems to update state to display correctly in the ItemShow route
+  // dispatching fetchItems to update state to display correctly in the CategoryList route
   dispatch(fetchItems());
   dispatch({ type: types.DELETE_CATEGORY, payload: id });
   history.push(`/items/${itemId}`);
