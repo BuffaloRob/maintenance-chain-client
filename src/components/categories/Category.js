e4208865e95e1f430df5d31c3ea4047a056eaa18
BuffaloRob <peckham.rob@gmail.com>
2019-05-25 22:31:23 -0400

    Category: changed Edit from using <link> to onClick, now receives editCategoryClick

--- src/components/categories/Category.js
+++ src/components/categories/Category.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Category = ({ category, selectCategory, match, itemId }) => {
-
-  const renderAdmin = item => (
+const Category = ({ category, selectCategory, match, itemId, editCategoryClick }) => {
+  const renderAdmin = category => (
     <div className='right floated content'>
-      <Link className='ui button primary' to={`/item/${item.id}/category/${category.id}/edit`}>Edit</Link>
-      <Link className='ui button negative' to={`/item/${item.id}/category/${category.id}/delete`}>Delete</Link>
+      <button className='ui button primary' onClick={() => editCategoryClick(category.id, itemId)}>Edit</button>
+      {/* <Link className='ui button primary' to={`/item/${item.id}/category/${category.id}/edit`}>Edit</Link> */}
+      <Link className='ui button negative' to={`/item/${itemId}/category/${category.id}/delete`}>Delete</Link>
     </div>
   );
 
