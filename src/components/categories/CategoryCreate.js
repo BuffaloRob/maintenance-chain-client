b1b4de899b65590c2a45e9d410e10e9d259a85e9
BuffaloRob <peckham.rob@gmail.com>
2019-05-25 22:29:56 -0400

    CategoryEdit & CategoryCreate: made them their own form, no longer passing to CategoryForm

--- src/components/categories/CategoryCreate.js
+++ src/components/categories/CategoryCreate.js
@@ -1,25 +1,103 @@
 import React from "react";
+import _ from 'lodash';
 import { connect } from 'react-redux';
 
-import CategoryForm from './CategoryForm';
-import { createCategory } from '../../actions/categoryActions';
-//need to import Connect() and wire up to action creator
+import { createCategory, fetchCategories } from '../../actions/categoryActions';
+import CategoryForm from "./CategoryForm";
+
+import { Field, reduxForm } from 'redux-form'
 
 class CategoryCreate extends React.Component {
 
-  onSubmit = (formValues, itemId) => {
-    itemId = this.props.match.params.id;
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return <div className='header'>{error}</div>;
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ui input ${meta.error && meta.touched ? 'error' : ''}`
+    return <>
+      {/* <label>{label}</label> */}
+      <div className={className}>
+        <input {...input} autoComplete="off" placeholder={label} />
+      </div>
+      <div className='ui error message'>
+        {this.renderError(meta)}
+      </div>
+    </>;
+  }
+
+  onSubmit = formValues => {
+    const itemId = this.props.match.params.itemId
     this.props.createCategory(formValues, itemId);
   }
 
   render() {
     return (
-      <div>
-        <h3>Create a New Category</h3>
-        <CategoryForm onSubmit={this.onSubmit} />
-      </div>
+      <>
+        <h3>Make a New Category</h3>
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form error'>
+          <Field
+            name='category[name]'
+            component={this.renderInput}
+            label='Enter Category Name'
+          />
+          <button className='ui button primary'>Submit</button>
+        </form>
+      </>
     );
   }
 }
 
-export default connect(null, { createCategory })(CategoryCreate);
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.name) {
+    errors.name = "You Must Enter an Item Name"
+  }
+
+  return errors;
+}
+
+CategoryCreate = reduxForm({
+  form: 'categoryForm',
+  validate: validate
+})(CategoryCreate);
+
+export default connect(null, { createCategory })(CategoryCreate)
+
+// import React from "react";
+// import { connect } from 'react-redux';
+
+// import CategoryForm from './CategoryForm';
+// import { createCategory } from '../../actions/categoryActions';
+// //need to import Connect() and wire up to action creator
+
+// class CategoryCreate extends React.Component {
+
+//   onSubmit = (formValues, itemId) => {
+//     debugger
+//     itemId = this.props.item.id;
+//     this.props.createCategory(formValues, itemId);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h3>Create a New Category</h3>
+//         <CategoryForm onSubmit={this.onSubmit} />
+//       </div>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state, ownProps) => {
+//   return {
+//     item: state.items[ownProps.match.params.itemId]
+//   }
+// };
+
+
+// export default connect(mapStateToProps, { createCategory })(CategoryCreate);
+
--- src/components/categories/CategoryEdit.js
+++ src/components/categories/CategoryEdit.js
@@ -2,43 +2,130 @@ import React from "react";
 import _ from 'lodash';
 import { connect } from 'react-redux';
 
-import { editCategory } from '../../actions/categoryActions';
+import { editCategory, fetchCategories } from '../../actions/categoryActions';
 import CategoryForm from "./CategoryForm";
+
+import { Field, reduxForm } from 'redux-form'
 
 class CategoryEdit extends React.Component {
 
-  componentDidMount() {
-    // this.props.fetchItem(this.props.match.params.id);
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return <div className='header'>{error}</div>;
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ui input ${meta.error && meta.touched ? 'error' : ''}`
+    return <>
+      {/* <label>{label}</label> */}
+      <div className={className}>
+        <input {...input} autoComplete="off" placeholder={label} />
+      </div>
+      <div className='ui error message'>
+        {this.renderError(meta)}
+      </div>
+    </>;
   }
 
   onSubmit = formValues => {
     const itemId = this.props.match.params.itemId
-    this.props.editCategory(formValues, this.props.match.params.id, itemId );
+    const catId = this.props.match.params.id
+    this.props.editCategory(formValues, catId , itemId);
   }
 
   render() {
-    if (!this.props.category) {
-      return <div>Loading...</div>
-    }
-
     return (
-      <div>
-        <h3>Edit {this.props.category.name}</h3>
-        <CategoryForm
-          onSubmit={this.onSubmit}
-        // initialValues={_.pick(this.props.item, 'name')}
+      <>
+      <h3>Edit {this.props.category.name}</h3>
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form error'>
+        <Field
+          name='category[name]'
+          component={this.renderInput}
+          label='Enter Category Name'
         />
-      </div>
-    )
+        <button className='ui button primary'>Submit</button>
+      </form>
+      </>
+    );
+  }
+}
+
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.name) {
+    errors.name = "You Must Enter an Item Name"
   }
 
+  return errors;
 }
 
+CategoryEdit = reduxForm({
+  form: 'categoryForm',
+  validate: validate
+})(CategoryEdit);
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    category: state.categories[ownProps.match.params.id]
-  };
-}
+export default connect(null, { editCategory })(CategoryEdit)
 
-export default connect(mapStateToProps, { editCategory })(CategoryEdit);
+///////
+// const CategoryEdit = ({ category, match }) => {
+//   const onSubmit = formValues => {
+//     const itemId = match.params.itemId
+//     this.props.editCategory(formValues, match.params.id, itemId);
+//   }
+
+  
+//     // if (!this.props.category) {
+//     //   return <div>Loading...</div>
+//     // }
+//   return ( 
+//     <div>
+//       <h3>Edit {category.name}</h3>
+//       <CategoryForm
+//         onSubmit={onSubmit}
+//         // initialValues={_.pick(this.props.item, 'name')}
+//       />
+//     </div>
+//   )
+
+// }
+
+// export default connect(null, { editCategory })(CategoryEdit)
+////////
+// class CategoryEdit extends React.Component {
+
+//   // componentDidMount() {
+//   //   this.props.fetchCategories(this.props.match.params.itemId)
+//   // }
+
+  // onSubmit = formValues => {
+  //   const itemId = this.props.match.params.itemId
+  //   debugger
+  //   this.props.editCategory(formValues, this.props.match.params.id, itemId );
+  // }
+
+//   render() {
+//     if (!this.props.category) {
+//       return <div>Loading...</div>
+//     }
+//     return (
+//       <div>
+//         <h3>Edit {this.props.category.name}</h3>
+//         <CategoryForm
+//           onSubmit={this.onSubmit}
+//           initialValues={_.pick(this.props.item, 'name')}
+//         />
+//       </div>
+//     )
+//   }
+
+// }
+
+// const mapStateToProps = (state, ownProps) => {
+//   return {
+//     category: state.categories[ownProps.match.params.id]
+//   };
+// }
+
+// export default connect(null, { editCategory })(CategoryEdit);
