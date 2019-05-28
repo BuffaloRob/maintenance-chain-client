1c324beaf1fc4b5f8cae3e8314c79cddd00e2c94
BuffaloRob <peckham.rob@gmail.com>
2019-05-28 12:56:08 -0400

    index: + theme and cssBaseline

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ import { PersistGate } from 'redux-persist/integration/react';
 import App from "./components/App";
 import rootReducer from './reducers'
 
+import { ThemeProvider} from '@material-ui/styles';
+import theme from './ui/theme';
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 const persistConfig = {
   key: 'root',
   storage,
@@ -30,7 +34,10 @@ export const persistor = persistStore(store);
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <App />
+      <ThemeProvider theme={theme} >
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </PersistGate>
   </Provider>,
   document.querySelector("#root")
