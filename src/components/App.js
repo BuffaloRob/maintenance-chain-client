9839fc1d4235af85009f32bfc19d8cfc3051738b
BuffaloRob <peckham.rob@gmail.com>
2019-05-28 20:50:00 -0400

    App: cleaned up and removed unneeded spaces

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch, withRouter } from 'react-router-dom';
 
 import history from '../history';
-
 import Header from './Header';
-
 import Home from './Home';
 import SignUp from './SignUp';
 import Login from './Login';
 import PastDue from './PastDue';
-
 import MaintenanceContainer from '../containers/MaintenanceContainer';
-
 
 class App extends React.Component {
 
@@ -23,16 +19,14 @@ class App extends React.Component {
           <>
             <Header />
             <Switch>
-              
               <Route exact path="/" component={Home} />
               <Route path="/signup" component={SignUp} />
               <Route path="/login" component={Login} />
               <Route path='/pastdue' component={PastDue} />
-              < MaintenanceContainer />
+              <MaintenanceContainer />
             </Switch>
           </>
         </Router>
-        
       </div>
     )
   }
@@ -44,4 +38,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, {})(App);
+export default withRouter(connect(mapStateToProps, {})(App));
