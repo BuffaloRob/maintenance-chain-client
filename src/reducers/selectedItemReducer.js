10bfe4184b3c095872210227e7ba3727339c26b8
BuffaloRob <peckham.rob@gmail.com>
2019-05-28 20:47:37 -0400

    selectedItemReducer: + case SELECT_ITEM_FOR_EDITED_CATEGORY

--- src/reducers/selectedItemReducer.js
+++ src/reducers/selectedItemReducer.js
@@ -1,8 +1,10 @@
-import { SELECT_ITEM } from '../actions/types';
+import { SELECT_ITEM, SELECT_ITEM_FOR_EDITED_CATEGORY } from '../actions/types';
 
 export default (state = {}, action) => {
   switch (action.type) {
     case SELECT_ITEM:
+      return Object.assign({}, state, action.payload)
+    case SELECT_ITEM_FOR_EDITED_CATEGORY:
       return Object.assign({}, state, action.payload)
     default:
       return state
