0dec260891343eeb12383bcd05831ad90d821957
BuffaloRob <peckham.rob@gmail.com>
2019-05-30 10:55:30 -0400

    LogList: using passed down item & catId to filter out the logs needed from selectedItem in state

--- src/components/logs/LogList.js
+++ src/components/logs/LogList.js
@@ -2,21 +2,32 @@ import React from "react";
 
 import Log from '../logs/Log';
 
-const LogList = ({ category, selectLog, match }) => {
-  const renderList = category.logs.map(log => (
+const LogList = ({ category, selectLog, match, item }) => {
+  debugger
+  const filteredLogs = item.logs.filter(log => log.category_id == category.id)
+  const renderList = filteredLogs.map(log => (
     <Log
       key={log.id}
       log={log}
       selectLog={selectLog}
       match={match}
-      itemId={category.item.id}
       categoryId={category.id}
     />
   ));
+  // const renderList = category.logs.map(log => (
+  //   <Log
+  //     key={log.id}
+  //     log={log}
+  //     selectLog={selectLog}
+  //     match={match}
+  //     categoryId={category.id}
+  //   />
+  // ));
 
   return (
     <div>
-      <h2>Logs for {category.name}</h2>
+      <h2>Logs for cat</h2>
+      {/* <h2>Logs for {item.categories[categoryId].name}</h2> */}
       <div className='ui celled list'>{renderList}</div>
     </div>
   )
