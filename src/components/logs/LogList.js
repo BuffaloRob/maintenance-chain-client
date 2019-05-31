74b00356795175d389a32ac4dc2e6563b2083fc1
BuffaloRob <peckham.rob@gmail.com>
2019-05-31 10:47:51 -0400

    LogList: now renders correctly, + link for create new log

--- src/components/logs/LogList.js
+++ src/components/logs/LogList.js
@@ -1,34 +1,30 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 import Log from '../logs/Log';
 
-const LogList = ({ categoryId, selectLog, match, item }) => {
-  debugger
-  const filteredLogs = item.logs.filter(log => log.category_id == categoryId)
-  const renderList = filteredLogs.map(log => (
+const LogList = ({ category, selectLog, match, item }) => {
+
+  const logs = item.logs.filter(log => (log.id === category[0].id))
+  const renderList = logs.map(log => (
     <Log
       key={log.id}
       log={log}
       selectLog={selectLog}
       match={match}
-      categoryId={categoryId}
+      categoryId={category.id}
     />
   ));
-  // const renderList = category.logs.map(log => (
-  //   <Log
-  //     key={log.id}
-  //     log={log}
-  //     selectLog={selectLog}
-  //     match={match}
-  //     categoryId={category.id}
-  //   />
-  // ));
 
   return (
     <div>
-      <h2>Logs for cat</h2>
-      {/* <h2>Logs for {item.categories[categoryId].name}</h2> */}
+      {/* <h2>Logs for cat</h2> */}
+      <h2>Logs for {category[0].name}</h2>
       <div className='ui celled list'>{renderList}</div>
+      <div style={{ textAlign: 'right' }} >
+        <Link to={`/item/${item.id}/category/${category[0].id}/log/new`} className='ui button primary'>Create New Log</Link>
+      </div>
+ 
     </div>
   )
 }
