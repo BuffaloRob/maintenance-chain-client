6ad8daff9b1418815f1ffc0f93cd53b4f057305f
BuffaloRob <peckham.rob@gmail.com>
2019-05-31 16:37:44 -0400

    selectedItemReducer: + comment on thinking out problem currently facing

--- src/reducers/selectedItemReducer.js
+++ src/reducers/selectedItemReducer.js
@@ -7,8 +7,31 @@ export default (state = {}, action) => {
     case SELECT_ITEM_FOR_EDITED_CATEGORY:
       return Object.assign({}, state, action.payload)
     case GET_ITEM:
+      // return { ...state, [action.payload.id]: action.payload };
       return Object.assign({}, state, action.payload)
     default:
       return state
   }
 }
+
+// I need to dispatch an action in this reducer that will update the state using data that is formatted differently than what is coming back in the payload
+// State structure => an object
+// selectedItem:
+//   {
+//     id: 3
+//     name: "test Item"
+//     user: {user obj}
+//     logs: []
+//     categories[{}] //an array of objects, the numbers 0 & 1 refer to the array index
+//       0: {id: 5, name: "cat5's name"}
+//       1: {id: 6, name: "another Cat"}
+//    }
+
+
+// payload structure => an object
+// {
+//   id: 5
+//   name: "cat5's name"
+//   item: {id: 3, name:"test Item"}
+//   logs: []
+// }
