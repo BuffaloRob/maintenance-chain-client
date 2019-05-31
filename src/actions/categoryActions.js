0ffed628baa6e6b1e119e0ea1f98508a72fb37ef
BuffaloRob <peckham.rob@gmail.com>
2019-05-31 16:41:02 -0400

    categoryActions: experimenting with ways to edit selectedCategory prop via editCategory

--- src/actions/categoryActions.js
+++ src/actions/categoryActions.js
@@ -2,7 +2,7 @@ import history from '../history';
 import apiURL from '../apis/maintenance';
 import * as types from './types';
 
-import { selectItemOnCatEdit } from '../actions/selectActions';
+import { getItem } from '../actions/selectActions';
 import { fetchItems } from '../actions/itemActions';
 
 export const createCategory = (formValues, itemId) => async dispatch => {
@@ -14,6 +14,7 @@ export const createCategory = (formValues, itemId) => async dispatch => {
 };
 
 export const fetchCategories = itemId => async dispatch => {
+  debugger
   const response = await apiURL.get(`/items/${itemId}/categories`);
   dispatch({ type: types.FETCH_CATEGORIES, payload: response.data });
 }
@@ -25,16 +26,22 @@ export const fetchCategory = (id, itemId) => async dispatch => {
 
 export const editCategory = (formValues, id, itemId) => async dispatch => {
   const response = await apiURL.put(`/items/${itemId}/categories/${id}`, formValues);
-  await dispatch(fetchItems());
-  await dispatch(selectItemOnCatEdit(parseInt(itemId)))
-  dispatch({ type: types.EDIT_CATEGORY, payload: response.data });
+  // await dispatch(fetchItems());
+  // await dispatch(selectItemOnCatEdit(parseInt(itemId)))
+  // debugger
+  await dispatch({ type: types.GET_ITEM, payload: response.data })
+
+  await dispatch(fetchItems())
+  // dispatch({ type: types.EDIT_CATEGORY, payload: response.data });
+  // await dispatch(getItem(itemId))
+
   history.push(`/items/${itemId}`);
 }
 
 export const deleteCategory = (id, itemId) => async dispatch => {
   await apiURL.delete(`/items/${itemId}/categories/${id}`);
   // dispatching fetchItems to update state to display correctly in the CategoryList route
-  dispatch(fetchItems());
+  // dispatch(fetchItems());
   dispatch({ type: types.DELETE_CATEGORY, payload: id });
   history.push(`/items/${itemId}`);
 }
