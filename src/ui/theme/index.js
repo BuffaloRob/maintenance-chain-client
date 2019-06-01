7aca453bb4130e67454ecf9b4eeac3cccda9ad83
BuffaloRob <peckham.rob@gmail.com>
2019-06-01 19:59:55 -0400

    theme index: changed to dark theme

--- src/ui/theme/index.js
+++ src/ui/theme/index.js
@@ -1,17 +1,29 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-
 const palette = {
+  type: 'dark',
   primary: { main: '#F4511E' },
   secondary: { main: '#78909C' }
 };
 
-const background = {
-  background: '#78909C'
-}
+// const background = {
+//   backgroundColor: '#78909C'
+// }
+
+// const paper = {
+//   paper: {
+//     backgroundColor: '#78909C'
+//   }
+// }
+
+// const container = {
+//   MuiContainer: {
+//     background: '#78909C'
+//   }
+// }
 
 const themeName = 'Orange and Gray';
 
-const theme = createMuiTheme({ palette, background, themeName });
+const theme = createMuiTheme({ palette, themeName });
 
 export default theme
