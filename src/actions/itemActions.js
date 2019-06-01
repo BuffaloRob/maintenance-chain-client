6fdfb3a2ab33ae66a51c10a10e9f5e93c571a6f3
BuffaloRob <peckham.rob@gmail.com>
2019-06-01 10:03:40 -0400

    itemActions: changed fetchItems() from axios call to fetch to stop bug where it doesnt always successfully fetchItems due to jwt decode error on api

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -2,7 +2,7 @@ import history from '../history';
 import apiURL from '../apis/maintenance';
 import * as types from './types';
 
-// const API = "http://localhost:3000/api/v1"
+const API = "http://localhost:3000/api/v1"
 
 export const createItem = formValues => async (dispatch, getState) => {
   const user_id = getState().auth.currentUser.user_id;
@@ -10,11 +10,6 @@ export const createItem = formValues => async (dispatch, getState) => {
   dispatch({ type: types.CREATE_ITEM, payload: response.data });
   history.push('/items');
 };
-
-export const fetchItems = () => async dispatch => {
-  const response = await apiURL.get('/items');
-  dispatch({ type: types.FETCH_ITEMS, payload: response.data });
-}
 
 export const fetchItem = id => async dispatch => {
   const response = await apiURL.get(`/items/${id}`);
@@ -33,28 +28,36 @@ export const deleteItem = id => async dispatch => {
   history.push('/items');
 }
 
-// export const fetchItems = () => {
-//   let data = {
-//     method: 'GET',
-//     headers: {
-//       'Accept': 'application/json',
-//       'Content-Type': 'application/json',
-//       'Authorization': 'Bearer ' + sessionStorage.jwt
-//       // 'Authorization': sessionStorage.jwt
-//     }
-//   }
+export const fetchItems = () => {
+  let data = {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + sessionStorage.jwt
+      // 'Authorization': sessionStorage.jwt
+    }
+  }
 
-//   return dispatch => {
-//     fetch(`${API}/items`, data)
-//       .then(resp => resp.json())
-//       .then(resp => {
-//         dispatch({
+  return dispatch => {
+    fetch(`${API}/items`, data)
+      .then(resp => resp.json())
+      .then(resp => {
+        dispatch({
 
-//           type: types.FETCH_ITEMS,
-//           payload: resp
-//         })
-//       })
-//       .catch(err => err)
+          type: types.FETCH_ITEMS,
+          payload: resp
+        })
+      })
+      .catch(err => err)
+  }
+}
+
+// export const fetchItems = () => async dispatch => {
+//   try {
+//     const response = await apiURL.get('/items');
+//     dispatch({ type: types.FETCH_ITEMS, payload: response.data })
+//   } catch (error) {
+//     console.log(error)
 //   }
 // }
-
