e602d846872b9185b8ee080bb3e9a71e61f967a8
BuffaloRob <peckham.rob@gmail.com>
2019-06-01 20:22:44 -0400

    Item: changed edit & delete buttons into icons

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -3,10 +3,11 @@ import { Link as RouterLink } from 'react-router-dom';
 import history from '../../history';
 import { Route } from 'react-router-dom';
 import ItemEdit from './ItemEdit';
-import { ListItem, Container, ListItemText, ListItemAvatar, Box, Divider, Avatar, ListItemSecondaryAction } from '@material-ui/core';
+import { ListItem, Container, ListItemText, ListItemAvatar, Box, Divider, Avatar, ListItemSecondaryAction, Fab, Icon } from '@material-ui/core';
 import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button'
 import Build from '@material-ui/icons/Build';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 
 const Item = ({ item, selectItem, editItemClick }) => {
@@ -14,8 +15,24 @@ const Item = ({ item, selectItem, editItemClick }) => {
   const renderAdmin = item => (
 
     <Container >
-      <Button component={RouterLink} to={`/item/${item.id}/edit`}>Edit</Button>
-      <Button component={RouterLink} to={`/item/${item.id}/delete`}>Delete</Button>
+      <Fab 
+        color="secondary" 
+        size="medium"
+        aria-label="Edit" 
+        component={RouterLink} 
+        to={`/item/${item.id}/edit`}
+      >
+        <Icon>edit_icon</Icon>
+      </Fab>
+      <Fab
+        color="primary"
+        size="medium"
+        aria-label="Delete"
+        component={RouterLink}
+        to={`/item/${item.id}/delete`}
+      >
+        <DeleteIcon />
+      </Fab>
     </Container>
 
   );
