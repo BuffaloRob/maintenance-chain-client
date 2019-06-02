db40f4221c2d785d25846fbca0905f8e5b495b96
BuffaloRob <peckham.rob@gmail.com>
2019-06-02 15:32:41 -0400

    LogCreate: now using material ui, date fields need to be fixed so they dont double print

--- src/components/logs/LogCreate.js
+++ src/components/logs/LogCreate.js
@@ -1,11 +1,32 @@
 import React from "react";
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form'
+import { TextField, Button, Box, InputAdornment } from "@material-ui/core";
 
 import LogForm from './LogForm';
 import { createLog } from '../../actions/logActions';
-//need to import Connect() and wire up to action creator
 
 class LogCreate extends React.Component {
+
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return <div className='header'>{error}</div>;
+    }
+  }
+
+  renderInput = ({ input, label, meta, type, variant, inputProps, inputLabelProps, rows }) => (
+    <TextField
+      label={label}
+      autoComplete="off"
+      type={type}
+      variant={variant}
+      // inputProps={{inputProps}}
+      // inputLabelProps={{inputLabelProps}}
+      rows={rows}
+      {...input}
+      margin="normal"
+    />
+  )
 
   onSubmit = (formValues) => {
     const itemId = this.props.match.params.itemId;
@@ -17,13 +38,71 @@ class LogCreate extends React.Component {
 
   render() {
     return (
-      <div>
-        <h3>Create a New Item to Track</h3>
-        <LogForm onSubmit={this.onSubmit} />
-      </div>
-    );
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form error'>
+        <Field
+          name='log[date_performed]'
+          type='date'
+          component={this.renderInput}
+          label='Date Performed'
+          variant='outlined'
+          inputlabelprops={{
+            shrink: true,
+            variant: 'outlined',
+          }}
+        /><br />
+        <Field
+          name='log[date_due]'
+          type='date'
+          component={this.renderInput}
+          label='Date Due'
+        /><br />
+        <Field
+          name='log[cost]'
+          type='number'
+          component={this.renderInput}
+          label='Cost $'
+          inputProps={{
+            startAdornment: (
+              < InputAdornment position="start" >
+                $
+              </InputAdornment>
+              ),
+          }}
+        /><br />
+        <Field
+          name='log[notes]'
+          type='text'
+          component={this.renderInput}
+          label='Notes'
+        /><br />
+        <Field
+          name='log[tools]'
+          type='text'
+          component={this.renderInput}
+          label='Tools Used'
+        /><br />
+        <Box>
+          <Button type='submit'>Submit</Button>
+        </Box>
+      </form>
+    )
   }
 }
+
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.name) {
+    errors.name = "You Must Enter an Item Name"
+  }
+
+  return errors;
+}
+
+LogCreate = reduxForm({
+  form: 'logForm',
+  validate: validate
+})(LogCreate);
 
 
 export default connect(null, { createLog })(LogCreate);
