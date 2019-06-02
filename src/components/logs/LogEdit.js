b72831b48fa7357d0db29efe0992a2da90d7dc31
BuffaloRob <peckham.rob@gmail.com>
2019-06-02 19:00:50 -0400

    LogEdit: changed to use material ui

--- src/components/logs/LogEdit.js
+++ src/components/logs/LogEdit.js
@@ -1,19 +1,38 @@
 import React from "react";
 import _ from 'lodash';
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form'
+import { TextField, Button, Box, InputAdornment } from "@material-ui/core";
 
 import { editLog } from '../../actions/logActions';
-import LogForm from "./LogForm";
 
 class LogEdit extends React.Component {
 
-  componentDidMount() {
-    // this.props.fetchItem(this.props.match.params.id);
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return <div className='header'>{error}</div>;
+    }
   }
 
+  renderInput = ({ input, label, meta, type, variant, inputProps, inputLabelProps, rows }) => (
+    <TextField
+      label={label}
+      autoComplete="off"
+      type={type}
+      variant={variant}
+      // inputProps={{inputProps}}
+      // inputLabelProps={{inputLabelProps}}
+      rows={rows}
+      {...input}
+      margin="normal"
+    />
+  )
+
   onSubmit = formValues => {
+    const logId = this.props.match.params.id
+    const catId = this.props.selectedLog.category_id
     const itemId = this.props.match.params.itemId
-    this.props.editLog(formValues, this.props.match.params.id, itemId);
+    this.props.editLog(formValues, logId, catId, itemId);
   }
 
   render() {
@@ -22,23 +41,78 @@ class LogEdit extends React.Component {
     }
 
     return (
-      <div>
-        <h3>Edit {this.props.log.date_performed}</h3>
-        <LogForm
-          onSubmit={this.onSubmit}
-        // initialValues={_.pick(this.props.item, 'name')}
-        />
-      </div>
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form error'>
+        <Field
+          name='log[date_performed]'
+          type='date'
+          component={this.renderInput}
+          label='Date Performed'
+          variant='outlined'
+          inputlabelprops={{
+            shrink: true,
+            variant: 'outlined',
+          }}
+        /><br />
+        <Field
+          name='log[date_due]'
+          type='date'
+          component={this.renderInput}
+          label='Date Due'
+        /><br />
+        <Field
+          name='log[cost]'
+          type='number'
+          component={this.renderInput}
+          label='Cost $'
+          inputProps={{
+            startAdornment: (
+              < InputAdornment position="start" >
+                $
+              </InputAdornment>
+            ),
+          }}
+        /><br />
+        <Field
+          name='log[notes]'
+          type='text'
+          component={this.renderInput}
+          label='Notes'
+        /><br />
+        <Field
+          name='log[tools]'
+          type='text'
+          component={this.renderInput}
+          label='Tools Used'
+        /><br />
+        <Box>
+          <Button type='submit'>Submit</Button>
+        </Box>
+      </form>
     )
   }
 
 }
 
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.name) {
+    errors.name = "You Must Enter an Item Name"
+  }
+
+  return errors;
+}
+
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    log: state.logs[ownProps.match.params.id]
+    selectedLog: state.selectedLog[0]
   };
 }
+
+LogEdit = reduxForm({
+  form: 'logForm',
+  validate: validate
+})(LogEdit)
 
 export default connect(mapStateToProps, { editLog })(LogEdit);
