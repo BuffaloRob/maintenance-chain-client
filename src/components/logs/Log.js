5aeb862afd1424ae1b627d9f37953e2b66a2ab17
BuffaloRob <peckham.rob@gmail.com>
2019-06-02 19:00:17 -0400

    Log: + <Dialog> for delete confirmation

--- src/components/logs/Log.js
+++ src/components/logs/Log.js
@@ -2,13 +2,27 @@ import React from 'react';
 import moment from 'moment';
 import { Link as RouterLink, withRouter } from 'react-router-dom';
 import { ListItem, Container, ListItemText, ListItemAvatar, Box, Divider, Avatar, ListItemSecondaryAction, Icon, Fab } from '@material-ui/core';
+import Button from '@material-ui/core/Button'
 import Build from '@material-ui/icons/Build';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 
-const Log = ({ log, selectLog, match, itemId, categoryId }) => {
+const Log = ({ log, selectLog, match, itemId, categoryId, deleteLogClick, editLogClick }) => {
 
   const formattedDate = moment(log.date_performed).format("MMM Do YYYY");
+  //Used in delete dialog pop up
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  }
+  const handleClose = () => {
+    setOpen(false);
+  }
 
   const renderAdmin = () => (
     <Container >
@@ -16,7 +30,7 @@ const Log = ({ log, selectLog, match, itemId, categoryId }) => {
         color="secondary"
         size="small"
         aria-label="Edit"
-        // onClick={editCategoryClick}
+        onClick={() => editLogClick(log.id, itemId)}
       >
         <Icon>edit_icon</Icon>
       </Fab>
@@ -24,11 +38,34 @@ const Log = ({ log, selectLog, match, itemId, categoryId }) => {
         color="primary"
         size="small"
         aria-label="Delete"
-        // component={RouterLink}
-        // to={`/item/${itemId}/category/${category.id}/delete`}
+        onClick={handleClickOpen}
       >
         <DeleteIcon />
       </Fab>
+
+      {/* Dialog code used for delete confirmation */}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{`Are you sure you want to delete the log for ${formattedDate}?`}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            You will lose all records associated with this log.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={() => deleteLogClick(log.id, categoryId, itemId)} color="primary" autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+
     </Container>
   );
   
