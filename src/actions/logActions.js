da28dcdde2c4fa680c153379a06cc5d93d9f94b9
BuffaloRob <peckham.rob@gmail.com>
2019-06-02 18:58:08 -0400

    logActions: updated editLog & deleteLog to new api routes

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -19,14 +19,14 @@ export const fetchLog = (id, itemId) => async dispatch => {
   dispatch({ type: types.FETCH_LOG, payload: response.data });
 }
 
-export const editLog = (formValues, id, itemId) => async dispatch => {
-  const response = await apiURL.put(`/items/${itemId}/logs/${id}`, formValues);
+export const editLog = (formValues, id, catId, itemId) => async dispatch => {
+  const response = await apiURL.put(`/items/${itemId}/categories/${catId}/logs/${id}`, formValues);
   dispatch({ type: types.EDIT_LOG, payload: response.data });
   history.push(`/items/${itemId}`);
 }
 
-export const deleteLog = (id, itemId) => async dispatch => {
-  await apiURL.delete(`/items/${itemId}/logs/${id}`);
+export const deleteLog = (id, catId, itemId) => async dispatch => {
+  await apiURL.delete(`/items/${itemId}/categories/${catId}/logs/${id}`);
   dispatch({ type: types.DELETE_LOG, payload: id });
   history.push(`/items/${itemId}`);
 }
