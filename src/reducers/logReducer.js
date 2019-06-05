4a3d89287405cc8cf5e16a6f3510e5bee834ca2f
BuffaloRob <peckham.rob@gmail.com>
2019-06-05 10:55:54 -0400

    logReducer: commented out because this is now handled in the selectedItemReducer

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,26 +1,26 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 
-import {
-  CREATE_LOG,
-  FETCH_LOG,
-  FETCH_LOGS,
-  DELETE_LOG,
-  EDIT_LOG,
-} from '../actions/types'
+// import {
+//   CREATE_LOG,
+//   FETCH_LOG,
+//   FETCH_LOGS,
+//   DELETE_LOG,
+//   EDIT_LOG,
+// } from '../actions/types'
 
-export default (state = {}, action) => {
-  switch (action.type) {
-    case FETCH_LOG:
-      return { ...state, [action.payload.id]: action.payload };
-    case CREATE_LOG:
-      return { ...state, [action.payload.id]: action.payload };
-    case EDIT_LOG:
-      return { ...state, [action.payload.id]: action.payload };
-    case DELETE_LOG:
-      return _.omit(state, action.payload);
-    case FETCH_LOGS:
-      return { ...state, ..._.mapKeys(action.payload, 'id') };
-    default:
-      return state;
-  }
-}
+// export default (state = {}, action) => {
+//   switch (action.type) {
+//     case FETCH_LOG:
+//       return { ...state, [action.payload.id]: action.payload };
+//     case CREATE_LOG:
+//       return { ...state, [action.payload.id]: action.payload };
+//     case EDIT_LOG:
+//       return { ...state, [action.payload.id]: action.payload };
+//     case DELETE_LOG:
+//       return _.omit(state, action.payload);
+//     case FETCH_LOGS:
+//       return { ...state, ..._.mapKeys(action.payload, 'id') };
+//     default:
+//       return state;
+//   }
+// }
