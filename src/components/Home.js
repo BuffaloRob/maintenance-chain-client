ad3e93e226439beb1827d88fa53c8fec8e30f325
BuffaloRob <peckham.rob@gmail.com>
2019-06-06 14:03:15 -0400

    Home:reformatted to make it easier to read

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {Link as RouterLink} from 'react-router-dom'
-import { Box, Typography, Button, Divider } from "@material-ui/core";
+import { Box, Typography, Divider } from "@material-ui/core";
+import Button from '@material-ui/core/Button'
+import { spacing } from '@material-ui/system';
 
 
 class Home extends React.Component {
@@ -9,11 +11,18 @@ class Home extends React.Component {
     return (
       <Box textAlign="center" >
         <Typography>
-          Welcome to Maintenance Chain. To get started create an <Button variant="outlined" color="primary" size="small" to={'/item/new'} component={RouterLink} >Item</Button> such as your car. Then make a category (oil change is a common example) to track. Once that's done just make a log entry for every oil change you perform.
+          Welcome to Maintenance Chain. To get started create an
+          <Button variant='text' color="primary" size="small" to={'/item/new'} component={RouterLink} >
+            Item
+          </Button> 
+          such as your car. Then make a category (oil change is a common example) to track. Once that's done just make a log entry for every oil change you perform.
         </Typography>
         <Divider/>
         <Typography>
-          And if you've been here before you can just go to your <Button variant="outlined" color="primary" size="small" to={'/items'} component={RouterLink}>Items</Button>
+          And if you've been here before you can just go to your  
+          <Button variant="text" color="primary" size="small" to={'/items'} component={RouterLink}>
+            Items
+          </Button>
         </Typography>
       </Box>
     );
