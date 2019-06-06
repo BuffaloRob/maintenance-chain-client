f7d2029b5f41353ec27cdea07a587448c6c53cc9
BuffaloRob <peckham.rob@gmail.com>
2019-06-06 10:50:12 -0400

    Home: added instructions

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,21 @@
 import React from "react";
+import {Link as RouterLink} from 'react-router-dom'
+import { Box, Typography, Button, Divider } from "@material-ui/core";
 
-//need to import Connect() and wire up to action creator
 
 class Home extends React.Component {
 
-  onSubmit = formValues => {
-    this.props.createItem(formValues);
-  }
-
   render() {
     return (
-      <div>
-        <h3>Welcome To Home Page</h3>
-        
-      </div>
+      <Box textAlign="center" >
+        <Typography>
+          Welcome to Maintenance Chain. To get started create an <Button variant="outlined" color="primary" size="small" to={'/item/new'} component={RouterLink} >Item</Button> such as your car. Then make a category (oil change is a common example) to track. Once that's done just make a log entry for every oil change you perform.
+        </Typography>
+        <Divider/>
+        <Typography>
+          And if you've been here before you can just go to your <Button variant="outlined" color="primary" size="small" to={'/items'} component={RouterLink}>Items</Button>
+        </Typography>
+      </Box>
     );
   }
 }
