28bee3cacd78e592ea8112fbb4f71446dbe6bdb0
BuffaloRob <peckham.rob@gmail.com>
2019-06-08 10:45:39 -0400

    RenderLoggedIn: added media queries to show menu icon when viewport doesnt show all menu items in single line

--- src/components/header/RenderLoggedIn.js
+++ src/components/header/RenderLoggedIn.js
@@ -1,27 +1,37 @@
 import React from 'react'
 import { Link as RouterLink } from 'react-router-dom';
 
-import { AppBar, Toolbar, Container } from '@material-ui/core';
+import { AppBar, Toolbar, Container, IconButton } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button'
+import MediaQuery from 'react-responsive';
+import MenuIcon from '@material-ui/icons/Menu';
+
 
 const RenderLoggedIn = ({ currentUser, handleLogout }) => {
   return (
     <AppBar position="static">
       <Toolbar>
         <Container >
-          <Button component={RouterLink} to="/items" color="inherit">
-            Items
-          </Button>
-          <Button component={RouterLink} to="/" color="inherit">
-            Home
-          </Button>
-          <Button onClick={e => handleLogout(e)} color="inherit">
-            Log Out
-          </Button>
+          <MediaQuery minDeviceWidth={540}>
+            <Button component={RouterLink} to="/items" color="inherit">
+              Items
+            </Button>
+            <Button component={RouterLink} to="/" color="inherit">
+              Home
+            </Button>
+            <Button onClick={e => handleLogout(e)} color="inherit">
+              Log Out
+            </Button>
+          </MediaQuery>
+          <MediaQuery maxDeviceWidth={539}>
+            <IconButton edge="start" >
+              <MenuIcon />
+            </IconButton>
+          </MediaQuery>
         </Container >
         <Container align="right">
-          <Typography >
+          <Typography variant='b1' noWrap>
             Welcome {currentUser.email}
           </Typography>
         </Container>
