6566734066e91270f239cc3086a5ef45475ca2f7
BuffaloRob <peckham.rob@gmail.com>
2019-06-09 10:32:35 -0400

    selectActions: removed unused actions

--- src/actions/selectActions.js
+++ src/actions/selectActions.js
@@ -1,6 +1,4 @@
-import { SELECT_ITEM, SELECT_CATEGORY, SELECT_LOG, UPDATE_SELECTED_ITEM_ON_CAT_EDIT, SELECT_CATEGORY_ID } from './types';
-import apiURL from '../apis/maintenance';
-
+import { SELECT_ITEM, SELECT_CATEGORY, SELECT_LOG } from './types';
 
 export const itemSelector = item => dispatch => {
   console.log(`item Selected: ${item.id}`)
@@ -13,14 +11,4 @@ export const categorySelector = (cat, itemId) => dispatch => {
 
 export const logSelector = (log, itemId) => dispatch => {
   dispatch({ type: SELECT_LOG, payload: log }) 
-}
-
-export const getItem = id => async dispatch => {
-  const response = await apiURL.get(`/items/${id}`);
-  // debugger
-  dispatch({ type: UPDATE_SELECTED_ITEM_ON_CAT_EDIT, payload: response.data });
-}
-
-export const categoryIdSelector = (catId, itemId) => dispatch => {
-  dispatch({ type: SELECT_CATEGORY_ID, payload: catId })
 }
