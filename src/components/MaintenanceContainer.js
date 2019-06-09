498bc14e897b9ce0c1dfca8c01488bbd5c509599
BuffaloRob <peckham.rob@gmail.com>
2019-06-09 10:58:30 -0400

    MaintenaceContainer: added PastDue route and selectPastDue()

--- src/components/MaintenanceContainer.js
+++ src/components/MaintenanceContainer.js
@@ -1,28 +1,24 @@
 import React from "react";
 import { connect } from 'react-redux';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { Typography, Box } from "@material-ui/core";
 
 import history from '../history';
-
 import ItemList from './items/ItemList';
 import ItemCreate from './items/ItemCreate';
 import ItemEdit from './items/ItemEdit';
-
 import CategoryCreate from './categories/CategoryCreate';
 import CategoryEdit from './categories/CategoryEdit';
 import CategoryList from './categories/CategoryList';
-
 import LogCreate from './logs/LogCreate';
 import LogEdit from './logs/LogEdit';
 import LogShow from './logs/LogShow';
 import LogList from './logs/LogList';
-
-
 import { fetchItems, deleteItem } from '../actions/itemActions';
 import { deleteCategory } from '../actions/categoryActions'
 import { deleteLog } from '../actions/logActions'
 import { itemSelector, categorySelector, logSelector } from '../actions/selectActions';
-import { Typography, Box } from "@material-ui/core";
+import PastDue from "./PastDue";
 
 class MaintenanceContainer extends React.Component {
   componentDidMount() {
@@ -75,6 +71,14 @@ class MaintenanceContainer extends React.Component {
     this.props.deleteLog(logId, itemId)
   }
 
+  selectPastDue = (logId, itemId) => {
+    const item = this.props.items[itemId] 
+    this.props.itemSelector(item)
+    const log = item.logs.filter(log => (log.id === logId))
+    this.props.logSelector(log)
+    history.push(`/log/${log[0].id}`)
+  }
+
   render() {
 
     if (this.props.isAuthenticated) {
@@ -82,20 +86,26 @@ class MaintenanceContainer extends React.Component {
         <div className="ui container">
           <>
             <Switch>
+              {/* Past Due */}
+              <Route exact path='/pastdue' render={props =>
+                <PastDue {...props}
+                  selectPastDue={this.selectPastDue}
+                />}
+              />
               {/* LogCreate */}
               <Route exact path='/item/:itemId/category/:id/log/new' component={LogCreate} />
               {/* LogShow */}
               <Route exact path='/log/:id' render={props =>
                 <LogShow {...props}
                   log={this.props.selectedLog}
-                  item={this.props.selectedItem}
+                  itemId={this.props.selectedItem.id}
                 />}
               />
               {/* LogEdit */}
               <Route exact path='/item/:itemId/log/:id/edit' component={LogEdit} />
               {/* CategoryCreate */}
               <Route exact path='/item/:itemId/category/new' component={CategoryCreate} />
-              {/* Category Show / LogList */}
+              {/* LogList / Category Show */}
               <Route exact path='/item/:itemId/category/:id' render={props =>
                 <LogList {...props}
                   category={this.props.selectedCategory}
@@ -120,7 +130,7 @@ class MaintenanceContainer extends React.Component {
               <Route exact path="/item/new" component={ItemCreate} />
               {/* ItemEdit */}
               <Route exact path="/item/:id/edit" component={ItemEdit} />
-              {/* Item Show / CategoryList */}
+              {/* CategoryList / Item Show */}
               <Route exact path="/item/:id" render={props =>
                 <CategoryList {...props}
                   item={this.props.selectedItem}
