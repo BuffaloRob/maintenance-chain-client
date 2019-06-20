93c67d44d833cca498f5d844b5c4f7c466d61f5b
BuffaloRob <peckham.rob@gmail.com>
2019-06-19 21:12:53 -0400

    itemActions: chnaged createItem from axios to fetch

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -2,15 +2,38 @@ import history from '../history';
 import apiURL from '../apis/maintenance';
 import * as types from './types';
 
-// const API = "http://localhost:3000/api/v1"
+const API = "http://localhost:3000/api/v1"
 // const API = 'https://maintenance-chain-api.herokuapp.com/api/v1'
 
-export const createItem = formValues => async (dispatch, getState) => {
-  const user_id = getState().auth.currentUser.id;
-  const response = await apiURL.post('/items', { ...formValues, user_id });
-  dispatch({ type: types.CREATE_ITEM, payload: response.data });
-  history.push('/items');
-};
+// export const createItem = formValues => async (dispatch, getState) => {
+//   const user_id = getState().auth.currentUser.id;
+//   const response = await apiURL.post('/items', { ...formValues, user_id });
+//   dispatch({ type: types.CREATE_ITEM, payload: response.data });
+//   history.push('/items');
+// };
+
+export const createItem = formValues => {
+  return dispatch => {
+    fetch(`${API}/items`, { method:'POST',
+      body: JSON.stringify(formValues),
+      headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.jwt
+      }})
+      .then(resp => resp.json())
+      .then(resp => {
+        if (!resp.error) {
+          dispatch({
+            type: types.CREATE_ITEM,
+            payload: resp
+          })
+          history.push('/items');
+        }
+      })
+      .catch(err => err)
+  }
+}
 
 export const fetchItem = id => async dispatch => {
   const response = await apiURL.get(`/items/${id}`);
@@ -29,32 +52,32 @@ export const deleteItem = id => async dispatch => {
   history.push('/items');
 }
 
-export const fetchItems = () => async dispatch => {
-  const response = await apiURL.get('/items');
-  dispatch({ type: types.FETCH_ITEMS, payload: response.data })
-}
-
-// export const fetchItems = () => {
-//   let data = {
-//     method: 'GET',
-//     headers: {
-//       'Accept': 'application/json',
-//       'Content-Type': 'application/json',
-//       'Authorization': 'Bearer ' + sessionStorage.jwt
-//       // 'Authorization': sessionStorage.jwt
-//     }
-//   }
-//   return dispatch => {
-//     fetch(`${API}/items`, data)
-//       .then(resp => resp.json())
-//       .then(resp => {
-//         if (!resp.error) {
-//           dispatch({
-//             type: types.FETCH_ITEMS,
-//             payload: resp
-//           })
-//         }
-//       })
-//       .catch(err => err)
-//   }
+// export const fetchItems = () => async dispatch => {
+//   const response = await apiURL.get('/items');
+//   dispatch({ type: types.FETCH_ITEMS, payload: response.data })
 // }
+
+export const fetchItems = () => {
+  let data = {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.jwt
+      // 'Authorization': sessionStorage.jwt
+    }
+  }
+  return dispatch => {
+    fetch(`${API}/items`, data)
+      .then(resp => resp.json())
+      .then(resp => {
+        if (!resp.error) {
+          dispatch({
+            type: types.FETCH_ITEMS,
+            payload: resp
+          })
+        }
+      })
+      .catch(err => err)
+  }
+}
