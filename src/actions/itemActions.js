4922779f083caa50ace9d8deb7e145def9ac8e37
BuffaloRob <peckham.rob@gmail.com>
2019-06-20 08:53:58 -0400

    itemActions: replaced all local URL constants with .env const. switched editItem from axios to fetch

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -2,25 +2,25 @@ import history from '../history';
 import apiURL from '../apis/maintenance';
 import * as types from './types';
 
-const API = "http://localhost:3000/api/v1"
-// const API = 'https://maintenance-chain-api.herokuapp.com/api/v1'
-
 // export const createItem = formValues => async (dispatch, getState) => {
 //   const user_id = getState().auth.currentUser.id;
 //   const response = await apiURL.post('/items', { ...formValues, user_id });
 //   dispatch({ type: types.CREATE_ITEM, payload: response.data });
 //   history.push('/items');
 // };
+const HEADERS = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer ' + localStorage.jwt
+}
 
 export const createItem = formValues => {
   return dispatch => {
-    fetch(`${API}/items`, { method:'POST',
+    fetch(`${process.env.REACT_APP_API_URL}/items`, { 
+      method:'POST',
       body: JSON.stringify(formValues),
-      headers:{
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + localStorage.jwt
-      }})
+      headers: HEADERS
+    })
       .then(resp => resp.json())
       .then(resp => {
         if (!resp.error) {
@@ -35,15 +35,36 @@ export const createItem = formValues => {
   }
 }
 
-export const fetchItem = id => async dispatch => {
-  const response = await apiURL.get(`/items/${id}`);
-  dispatch({ type: types.FETCH_ITEM, payload: response.data });
-}
+// export const fetchItem = id => async dispatch => {
+//   const response = await apiURL.get(`/items/${id}`);
+//   dispatch({ type: types.FETCH_ITEM, payload: response.data });
+// }
 
-export const editItem = (id, formValues) => async dispatch => {
-  const response = await apiURL.put(`/items/${id}`, formValues);
-  dispatch({ type: types.EDIT_ITEM, payload: response.data });
-  history.push('/items');
+// export const editItem = (id, formValues) => async dispatch => {
+//   const response = await apiURL.put(`/items/${id}`, formValues);
+//   dispatch({ type: types.EDIT_ITEM, payload: response.data });
+//   history.push('/items');
+// }
+
+export const editItem = (id, formValues) => {
+  return dispatch => {
+    fetch(`${process.env.REACT_APP_API_URL}/items/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify(formValues),
+      headers: HEADERS
+    })
+      .then(resp => resp.json())
+      .then(resp => {
+        if (!resp.error) {
+          dispatch({
+            type: types.EDIT_ITEM,
+            payload: resp
+          })
+          history.push('/items');
+        }
+      })
+      .catch(err => err)
+  }
 }
 
 export const deleteItem = id => async dispatch => {
@@ -68,7 +89,7 @@ export const fetchItems = () => {
     }
   }
   return dispatch => {
-    fetch(`${API}/items`, data)
+    fetch(`${process.env.REACT_APP_API_URL}/items`, data)
       .then(resp => resp.json())
       .then(resp => {
         if (!resp.error) {
