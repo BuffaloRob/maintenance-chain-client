d7c6e36fd0694055779a133ab947515e24b155c9
BuffaloRob <peckham.rob@gmail.com>
2019-06-21 12:45:13 -0400

    categoryACtions: changed all axios calls to fetch so that website will pass jwt in auth header

--- src/actions/categoryActions.js
+++ src/actions/categoryActions.js
@@ -1,38 +1,78 @@
 import history from '../history';
-import apiURL from '../apis/maintenance';
 import * as types from './types';
 
 import { fetchItems } from '../actions/itemActions';
 
-export const createCategory = (formValues, itemId) => async dispatch => {
-  const response = await apiURL.post(`/items/${itemId}/categories`, { ...formValues});
-  // dispatching fetchItems to update state to display correctly in the CategoryList route
-  dispatch(fetchItems());
-  dispatch({ type: types.CREATE_CATEGORY, payload: response.data });  
-  history.push(`/item/${itemId}`);
-};
-
-export const fetchCategories = itemId => async dispatch => {
-  const response = await apiURL.get(`/items/${itemId}/categories`);
-  dispatch({ type: types.FETCH_CATEGORIES, payload: response.data });
+export const createCategory = (formValues, itemId) => {
+  return dispatch => {
+    fetch(`${process.env.REACT_APP_API_URL}/items/${itemId}/categories`, {
+      method: 'POST',
+      body: JSON.stringify(formValues),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.jwt
+      }
+    })  
+      .then(resp => resp.json())
+      .then(resp => {
+        if (!resp.error) {
+          dispatch({
+            type: types.CREATE_CATEGORY,
+            payload: resp
+          })
+          history.push(`/item/${itemId}`)
+        }
+      })
+      .then(() => dispatch(fetchItems()))
+      .catch(err => err)
+  }
 }
 
-export const fetchCategory = (id, itemId) => async dispatch => {
-  const response = await apiURL.get(`/items/${itemId}/categories/${id}`);
-  dispatch({ type: types.FETCH_CATEGORY, payload: response.data });
+export const editCategory = (formValues, id, itemId) => {
+  return dispatch => {
+    fetch(`${process.env.REACT_APP_API_URL}/items/${itemId}/categories/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify(formValues),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.jwt
+      }
+    })
+      .then(resp => resp.json())
+      .then(resp => {
+        if (!resp.error) {
+          dispatch({
+            type: types.EDIT_CATEGORY,
+            payload: resp
+          })
+          history.push(`/item/${itemId}`)
+        }
+      })
+      .then(() => dispatch(fetchItems()))
+      .catch(err => err)
+  }
 }
 
-export const editCategory = (formValues, id, itemId) => async dispatch => {
-  const response = await apiURL.put(`/items/${itemId}/categories/${id}`, formValues);
-  dispatch(fetchItems())
-  dispatch({ type: types.EDIT_CATEGORY, payload: response.data })
-  history.push(`/item/${itemId}`);
-}
-
-export const deleteCategory = (id, itemId) => async dispatch => {
-  await apiURL.delete(`/items/${itemId}/categories/${id}`);
-  // dispatching fetchItems to update state to display correctly in the CategoryList route
-  dispatch(fetchItems());
-  dispatch({ type: types.DELETE_CATEGORY, payload: id });
-  history.push(`/item/${itemId}`);
+export const deleteCategory = (id, itemId) => {
+  return dispatch => {
+    fetch(`${process.env.REACT_APP_API_URL}/items/${itemId}/categories/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.jwt
+      }
+    })
+      .then(() => {
+        dispatch({
+          type: types.DELETE_CATEGORY,
+          payload: id
+        })
+        history.push(`/item/${itemId}`);
+      })
+      .then(() => dispatch(fetchItems()))
+      .catch(err => err)
+  }
 }
