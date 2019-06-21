c5028f39526e86deb79095ac99283446b6e76771
BuffaloRob <peckham.rob@gmail.com>
2019-06-21 14:14:36 -0400

    queryReducer: + FETCH_UPCOMING

--- src/reducers/queryReducer.js
+++ src/reducers/queryReducer.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
 
-import { FETCH_PAST_DUE } from '../actions/types';
+import { FETCH_PAST_DUE, FETCH_UPCOMING } from '../actions/types';
 
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_PAST_DUE:
+      return { ...state, ..._.mapKeys(action.payload, 'id') };
+    case FETCH_UPCOMING:
       return { ...state, ..._.mapKeys(action.payload, 'id') };
     default:
       return state;
