2533d60f18af96f85dd9028e99c0e9c4a1680525
BuffaloRob <peckham.rob@gmail.com>
2019-06-21 12:53:24 -0400

    axios config: removed unused code

--- src/apis/maintenance.js
+++ src/apis/maintenance.js
@@ -1,17 +1,13 @@
 import axios from 'axios';
 
-const TOKEN = localStorage.getItem('jwt')
-
 // Set-up for the json-api server
 export default axios.create({
-  // baseURL: 'http://localhost:3000/api/v1',
-  baseURL: 'https://maintenance-chain-api.herokuapp.com/api/v1',
+  baseURL: 'http://localhost:3000/api/v1',
+  // baseURL: 'https://maintenance-chain-api.herokuapp.com/api/v1',
+  // withCredentials: true,
   headers: { 
     'Accept': 'application/json',
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${TOKEN}`
-    // 'Authorization': 'Bearer ' + sessionStorage.jwt
-    // 'Authorization': sessionStorage.jwt
-
+    'Authorization': 'Bearer ' + localStorage.jwt
   }
 })
