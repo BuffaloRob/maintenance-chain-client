ac244570daf2e5474285784d6793cd10c6ed56b1
BuffaloRob <peckham.rob@gmail.com>
2019-06-21 12:52:49 -0400

    queryActions: changed fetchPastDue from using axios to fetch

--- src/actions/queryActions.js
+++ src/actions/queryActions.js
@@ -1,9 +1,26 @@
 import history from '../history';
-import apiURL from '../apis/maintenance';
 import * as types from './types';
 
-export const fetchPastDue = () => async dispatch => {
-  const response = await apiURL.get('/past_due');
-  dispatch({ type: types.FETCH_PAST_DUE, payload: response.data });
-  history.push('/pastdue');
+export const fetchPastDue = () => {
+  return dispatch => {
+    fetch(`${process.env.REACT_APP_API_URL}/past_due`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.jwt
+      }
+    })
+      .then(resp => resp.json())
+      .then(resp => {
+        if (!resp.error) {
+          dispatch({
+            type: types.FETCH_PAST_DUE,
+            payload: resp
+          })
+          history.push(`/pastdue`)
+        }
+      })
+      .catch(err => err)
+  }
 }
