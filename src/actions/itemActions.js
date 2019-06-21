e0f5222cf0d2450f81e9800e8e27acd8261eaabb
BuffaloRob <peckham.rob@gmail.com>
2019-06-21 12:51:51 -0400

    itemActions: changed delete from using axios to fetch

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -1,25 +1,16 @@
 import history from '../history';
-import apiURL from '../apis/maintenance';
 import * as types from './types';
-
-// export const createItem = formValues => async (dispatch, getState) => {
-//   const user_id = getState().auth.currentUser.id;
-//   const response = await apiURL.post('/items', { ...formValues, user_id });
-//   dispatch({ type: types.CREATE_ITEM, payload: response.data });
-//   history.push('/items');
-// };
-const HEADERS = {
-  'Accept': 'application/json',
-  'Content-Type': 'application/json',
-  'Authorization': 'Bearer ' + localStorage.jwt
-}
 
 export const createItem = formValues => {
   return dispatch => {
     fetch(`${process.env.REACT_APP_API_URL}/items`, { 
       method:'POST',
       body: JSON.stringify(formValues),
-      headers: HEADERS
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.jwt
+      }
     })
       .then(resp => resp.json())
       .then(resp => {
@@ -35,23 +26,16 @@ export const createItem = formValues => {
   }
 }
 
-// export const fetchItem = id => async dispatch => {
-//   const response = await apiURL.get(`/items/${id}`);
-//   dispatch({ type: types.FETCH_ITEM, payload: response.data });
-// }
-
-// export const editItem = (id, formValues) => async dispatch => {
-//   const response = await apiURL.put(`/items/${id}`, formValues);
-//   dispatch({ type: types.EDIT_ITEM, payload: response.data });
-//   history.push('/items');
-// }
-
 export const editItem = (id, formValues) => {
   return dispatch => {
     fetch(`${process.env.REACT_APP_API_URL}/items/${id}`, {
       method: 'PUT',
       body: JSON.stringify(formValues),
-      headers: HEADERS
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.jwt
+      }
     })
       .then(resp => resp.json())
       .then(resp => {
@@ -67,16 +51,26 @@ export const editItem = (id, formValues) => {
   }
 }
 
-export const deleteItem = id => async dispatch => {
-  await apiURL.delete(`/items/${id}`);
-  dispatch({ type: types.DELETE_ITEM, payload: id });
-  history.push('/items');
+export const deleteItem = id => {
+  return dispatch => {
+    fetch(`${process.env.REACT_APP_API_URL}/items/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.jwt
+      }
+    })
+      .then(() => {
+        dispatch({
+          type: types.DELETE_ITEM,
+          payload: id
+        })
+        history.push('/items');
+      })
+      .catch(err => err)
+  }
 }
-
-// export const fetchItems = () => async dispatch => {
-//   const response = await apiURL.get('/items');
-//   dispatch({ type: types.FETCH_ITEMS, payload: response.data })
-// }
 
 export const fetchItems = () => {
   let data = {
@@ -85,7 +79,6 @@ export const fetchItems = () => {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ' + localStorage.jwt
-      // 'Authorization': sessionStorage.jwt
     }
   }
   return dispatch => {
