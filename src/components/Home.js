87e5aee3ad64810a771b8ae8320215e60eb81f75
BuffaloRob <peckham.rob@gmail.com>
2019-08-07 09:42:14 -0400

    Home: + background pic, + assets folder w/pic

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,27 +4,36 @@ import Box from "@material-ui/core/Box";
 import Divider from '@material-ui/core/Divider'
 import Typography from '@material-ui/core/Typography'
 import Link from '@material-ui/core/Link'
+import enginePic from '../assets/engine.JPG'
 
+const imageStyle = {
+  width: '100%',
+  height: 'auto',
+  backgroundImage: `url(${enginePic})`,
+  backgroundSize: 'cover'
+};
 class Home extends React.Component {
 
   render() {
     return (
-      <Box textAlign="center" >
-        <Typography>
-          Welcome to Maintenance Chain. To get started create an
-          <Link color="primary" to={'/item/new'} component={RouterLink} >
-            Item
-          </Link> 
-          such as your car. Then make a category (oil change is a common example) to track. Once that's done just make a log entry for every oil change you perform.
-        </Typography>
-        <Divider/>
-        <Typography>
-          And if you've been here before you can just go to your  
-          <Link to={'/items'} component={RouterLink}>
-            Items
-          </Link>
-        </Typography>
-      </Box>
+      <div style={imageStyle}>
+        <Box textAlign="center" >
+          <Typography>
+            Welcome to Maintenance Chain. To get started create an
+            <Link color="primary" to={'/item/new'} component={RouterLink} >
+              Item
+            </Link> 
+            such as your car. Then make a category (oil change is a common example) to track. Once that's done just make a log entry for every oil change you perform.
+          </Typography>
+          <Divider/>
+          <Typography>
+            And if you've been here before you can just go to your  
+            <Link to={'/items'} component={RouterLink}>
+              Items
+            </Link>
+          </Typography>
+        </Box>
+      </div>
     );
   }
 }
