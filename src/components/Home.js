287cae31995ee9bed96f693127f052cf602f9a35
BuffaloRob <peckham.rob@gmail.com>
2019-08-08 10:06:26 -0400

    Home: used withStyles to create background image on only the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,44 @@
 import React from "react";
-import {Link as RouterLink} from 'react-router-dom'
+import {Link as RouterLink} from 'react-router-dom';
 import Box from "@material-ui/core/Box";
-import Divider from '@material-ui/core/Divider'
-import Typography from '@material-ui/core/Typography'
-import Link from '@material-ui/core/Link'
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+import enginePic from '../assets/cobraMotor.jpg';
+import { withStyles } from "@material-ui/styles";
+
+// This adds the background pic to only the home page
+const styles = {
+  "@global": {
+    body: {
+      backgroundImage: `url(${enginePic})`,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center center",
+      backgroundSize: "cover",
+      backgroundAttachment: "fixed",
+      // height: "100%",
+    }
+  }
+}
 
 class Home extends React.Component {
+  
 
   render() {
+    const {classes} = this.props;
     return (
-      <Box textAlign="center" >
+      <Box textAlign="center" className={classes.body}>
         <Typography>
           Welcome to Maintenance Chain. To get started create an
           <Link color="primary" to={'/item/new'} component={RouterLink} >
             Item
-          </Link> 
+          </Link>
           such as your car. Then make a category (oil change is a common example) to track. Once that's done just make a log entry for every oil change you perform.
         </Typography>
         <Divider/>
         <Typography>
-          And if you've been here before you can just go to your  
+          And if you've been here before you can just go to your
           <Link to={'/items'} component={RouterLink}>
             Items
           </Link>
@@ -29,4 +48,4 @@ class Home extends React.Component {
   }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
