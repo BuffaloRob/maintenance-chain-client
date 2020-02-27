2774359e715bd3679b7c50ec29dfa61f6b51752a
BuffaloRob <peckham.rob@gmail.com>
2020-02-27 12:39:46 -0500

    styled side bar

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -3,17 +3,23 @@ import Drawer from '@material-ui/core/Drawer'
 
 export const StyledDrawer = styled(Drawer)`
   .MuiDrawer-paperAnchorLeft {
+    color: #F4511E;
     width: 25%;
     font-size: 30px;
-    padding: 30px;
+    padding-top: 40px;
+    // padding: 40px 0 0 30px;
     @media (max-width: 768px) {
       width: 70%;
-      align-items: center;
     }
   }
 
   .MuiListItem-root {
-    padding: 10px 0;
+    padding-left: 30px;
+    margin: 30px 0;
+    @media(max-width: 768px) {
+      padding-left: 40px;
+      margin: 40px 0;
+    }
   
   }
 `
