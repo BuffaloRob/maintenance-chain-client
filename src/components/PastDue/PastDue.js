aea3513ac5bfbaf93a62ffb321a7c854c450b824
BuffaloRob <peckham.rob@gmail.com>
2020-02-27 17:27:22 -0500

    styled upcoming and pastdue views

---
+++ src/components/PastDue/PastDue.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import List from '@material-ui/core/List';
+import moment from 'moment'
+
+import { fetchPastDue } from '../../actions/queryActions'
+import { StyledListItem } from './styles';
+
+class PastDue extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchPastDue();
+  }
+
+  renderList() {
+    return this.props.logs.map(log => {
+      const formattedDateDue = moment(log.date_due).format("MMM Do YYYY");
+      return (
+        <StyledListItem
+          key={log.id}
+          button
+          onClick={() => this.props.selectPastDue(log.id, log.category.item_id, log.category_id)}
+          divider
+        >
+          {log.category.name} was due on {formattedDateDue}
+        </StyledListItem>
+      )
+      
+    })
+  }
+  
+  render() {
+    return (
+      <Container>
+        <Typography variant="h2">
+          <Box textAlign="center">Past Due</Box>
+        </Typography>
+        <List component="nav">{this.renderList()}</List>
+      </Container>
+    )
+  }
+
+}
+
+const mapStateToProps = state => {
+  return {
+    logs: Object.values(state.pastDue),
+  }
+}
+
+export default connect(mapStateToProps, { fetchPastDue })(PastDue);
---
+++ src/components/Upcoming/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import ListItem from '@material-ui/core/ListItem';
+
+export const StyledListItem = styled(ListItem)`
+  font-size: 24px;
+  font-weight: 500;
+  color: #F4511E;
+  justify-content: center;
+  text-align: center;
+  align-content: center;
+  padding: 20px 0 40px;
+`
